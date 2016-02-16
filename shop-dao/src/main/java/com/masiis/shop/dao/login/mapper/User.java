48210a317a3093992beaacda2b352005ea2e8f7e
caitb <caitb@139.com>
2016-02-16 16:58:56 +0800

    resource

---
+++ shop-dao/src/main/java/com/masiis/shop/dao/login/mapper/User.java
@@ -0,0 +1,74 @@
+package com.masiis.shop.dao.login.mapper;
+
+/**
+ * Created by cai_tb on 16/2/16.
+ */
+public class User {
+
+    private Long id;
+    private String userName;
+    private String trueName;
+    private String sex;
+    private Integer age;
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getTrueName() {
+        return trueName;
+    }
+
+    public void setTrueName(String trueName) {
+        this.trueName = trueName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", trueName='" + trueName + '\'' +
+                ", sex='" + sex + '\'' +
+                ", age=" + age +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
---
+++ shop-dao/src/main/java/com/masiis/shop/dao/login/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.masiis.shop.dao.login.mapper;
+
+/**
+ * Created by cai_tb on 16/2/16.
+ */
+public interface UserMapper {
+
+    User findById(Long id);
+}

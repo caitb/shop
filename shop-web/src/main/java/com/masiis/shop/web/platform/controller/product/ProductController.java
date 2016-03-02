38718978210f71134b1cb738de034559564fe19a
zhaoliang <370787497@qq.com>
2016-03-02 15:50:42 +0800

    ++

--- shop-web/src/main/java/com/masiis/shop/web/platform/controller/product/ProductController.java
+++ shop-web/src/main/java/com/masiis/shop/web/platform/controller/product/ProductController.java
@@ -1,7 +1,9 @@
 package com.masiis.shop.web.platform.controller.product;
 
+import com.masiis.shop.web.platform.controller.base.BaseController;
+
 /**
  * @autor jipengkun
  */
-public class ProductController {
+public class ProductController extends BaseController {
 }

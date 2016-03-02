28c811b8fd9f905c93cbed05e4a13688b1ef052c
zhaoliang <370787497@qq.com>
2016-03-02 15:31:14 +0800

    service

---
+++ shop-web/src/main/java/com/masiis/shop/web/platform/service/order/COrderService.java
@@ -0,0 +1,7 @@
+package com.masiis.shop.web.platform.service.order;
+
+/**
+ * Created by ZhaoLiang on 2016/3/2.
+ */
+public class COrderService {
+}
---
+++ shop-web/src/main/java/com/masiis/shop/web/platform/service/user/UserService.java
@@ -0,0 +1,7 @@
+package com.masiis.shop.web.platform.service.user;
+
+/**
+ * Created by ZhaoLiang on 2016/3/2.
+ */
+public class UserService {
+}

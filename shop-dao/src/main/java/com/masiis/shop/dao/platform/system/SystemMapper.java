09cdb1a5072b8c369178176f286250dcbebe9a05
zhaoliang <370787497@qq.com>
2016-03-02 16:35:50 +0800

    +

---
+++ shop-dao/src/main/java/com/masiis/shop/dao/platform/system/SystemMapper.java
@@ -0,0 +1,7 @@
+package com.masiis.shop.dao.platform.system;
+
+/**
+ * Created by ZhaoLiang on 2016/3/2.
+ */
+public class SystemMapper {
+}
---
+++ shop-dao/src/main/java/com/masiis/shop/dao/platform/user/UserMapper.java
@@ -0,0 +1,7 @@
+package com.masiis.shop.dao.platform.user;
+
+/**
+ * Created by ZhaoLiang on 2016/3/2.
+ */
+public class UserMapper {
+}

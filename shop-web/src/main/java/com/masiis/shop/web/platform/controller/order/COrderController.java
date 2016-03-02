ca8f1f03dbcc03621cac77ee11cbbeb9a6a0541d
zhaoliang <370787497@qq.com>
2016-03-02 17:18:30 +0800

    +

---
+++ shop-web/src/main/java/com/masiis/shop/web/platform/controller/order/COrderController.java
@@ -0,0 +1,9 @@
+package com.masiis.shop.web.platform.controller.order;
+
+import com.masiis.shop.web.platform.controller.base.BaseController;
+
+/**
+ * Created by ZhaoLiang on 2016/3/2.
+ */
+public class COrderController extends BaseController {
+}

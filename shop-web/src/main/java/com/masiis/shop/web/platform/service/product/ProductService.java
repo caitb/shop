4b2710a71b9b58804d5142de1e777428e102a395
Tracy_jjh@163.com <jia13546550112>
2016-03-03 19:45:54 +0800

    后台部分代码提交

--- shop-web/src/main/java/com/masiis/shop/web/platform/service/product/ProductService.java
+++ shop-web/src/main/java/com/masiis/shop/web/platform/service/product/ProductService.java
@@ -1,7 +1,34 @@
 package com.masiis.shop.web.platform.service.product;
 
+import com.masiis.shop.dao.platform.product.ProductMapper;
+import com.masiis.shop.dao.beans.product.Product;
+import com.masiis.shop.dao.po.ComSkuImage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 /**
  * Created by ZhaoLiang on 2016/3/2.
  */
+@Service
+@Transactional
 public class ProductService {
+
+    @Autowired
+    private ProductMapper productMapper;
+
+    /**
+     * 根据商品ID展示商品属性详情
+     * @param skuId
+     */
+    public Product getSkuDetails(String skuId) throws Exception{
+        Product product = productMapper.getSkuDetailsBySkuId(skuId);
+        List<ComSkuImage> skuImgList = productMapper.getSkuImgById(skuId);
+        if(skuImgList!=null && skuImgList.size()>0){
+            product.setComSkuImages(skuImgList);
+        }
+        return product;
+    }
 }

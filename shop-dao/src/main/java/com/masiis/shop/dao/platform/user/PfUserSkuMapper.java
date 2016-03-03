583c735331889d814e9f5085795affa4dcb38628
zhaoliang <370787497@qq.com>
2016-03-03 18:23:51 +0800

    添加代理订单

---
+++ shop-dao/src/main/java/com/masiis/shop/dao/platform/user/PfUserSkuMapper.java
@@ -0,0 +1,23 @@
+/*
+ * PfUserSkuMapper.java
+ * Copyright(C) 2014-2016 麦士集团
+ * All rights reserved.
+ * -----------------------------------------------
+ * 2016-03-03 Created
+ */
+package com.masiis.shop.dao.platform.user;
+
+import com.masiis.shop.dao.po.PfUserSku;
+import java.util.List;
+
+public interface PfUserSkuMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PfUserSku record);
+
+    PfUserSku selectByPrimaryKey(Integer id);
+
+    List<PfUserSku> selectAll();
+
+    int updateByPrimaryKey(PfUserSku record);
+}
--- shop-web/src/main/java/com/masiis/shop/web/platform/service/order/BOrderService.java
+++ shop-web/src/main/java/com/masiis/shop/web/platform/service/order/BOrderService.java
@@ -1,12 +1,18 @@
 package com.masiis.shop.web.platform.service.order;
 
+import com.masiis.shop.common.exceptions.BusinessException;
+import com.masiis.shop.dao.platform.order.PfBorderConsigneeMapper;
 import com.masiis.shop.dao.platform.order.PfBorderItemMapper;
 import com.masiis.shop.dao.platform.order.PfBorderMapper;
+import com.masiis.shop.dao.platform.user.PfUserSkuMapper;
 import com.masiis.shop.dao.po.PfBorder;
+import com.masiis.shop.dao.po.PfBorderConsignee;
 import com.masiis.shop.dao.po.PfBorderItem;
+import com.masiis.shop.dao.po.PfUserSku;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.swing.plaf.nimbus.NimbusStyle;
 import java.util.List;
 
 /**
@@ -18,14 +24,35 @@ public class BOrderService {
     private PfBorderMapper bOrderMapper;
     @Resource
     private PfBorderItemMapper borderItemMapper;
+    @Resource
+    private PfBorderConsigneeMapper pfBorderConsigneeMapper;
+    @Resource
+    private PfUserSkuMapper pfUserSkuMapper;
+
     /**
      * 添加订单
+     *
      * @param pfBorder
      * @param pfBorderItems
      */
-    public void AddBOrder(PfBorder pfBorder, List<PfBorderItem> pfBorderItems){
+    public void AddBOrder(PfBorder pfBorder, List<PfBorderItem> pfBorderItems, PfUserSku pfUserSku) {
+        if (pfBorder == null) {
+            throw new BusinessException("pfBorder为空");
+        }
+        if (pfUserSku == null) {
+            throw new BusinessException("pfUserSku为空");
+        }
+        if (pfBorderItems == null || pfBorderItems.size() == 0) {
+            throw new BusinessException("pfBorderItems为空");
+        }
+        //添加订单
         bOrderMapper.insert(pfBorder);
-
-//        borderItemMapper.insert(pfBorderItems);
+        //添加订单商品
+        for (PfBorderItem pfBorderItem : pfBorderItems) {
+            pfBorderItem.setPfBorderId(pfBorder.getId());
+            borderItemMapper.insert(pfBorderItem);
+        }
+        //添加用户代理商品关系
+        pfUserSkuMapper.insert(pfUserSku);
     }
 }

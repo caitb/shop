9bb93512a6b747b0a1ccde478d0b66d16eec71d4
Tracy_jjh@163.com <jia13546550112>
2016-03-04 15:36:42 +0800

    前后端页面整合，部分细节需要优化

--- shop-dao/src/main/java/com/masiis/shop/dao/beans/product/Product.java
+++ shop-dao/src/main/java/com/masiis/shop/dao/beans/product/Product.java
@@ -3,19 +3,31 @@ package com.masiis.shop.dao.beans.product;
 import com.masiis.shop.dao.po.ComSku;
 import com.masiis.shop.dao.po.ComSkuImage;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
  * Created by ZhaoLiang on 2016/3/2.
  */
-public class Product extends ComSku{
+public class Product extends ComSku {
 
     private String spuName;
-    private String content;
+
+    private String content;//图文详情
+
     private String slogan;
+
     private Integer stock;//商品库存
 
     private List<ComSkuImage> comSkuImages;//商品图
+
+    private Boolean isPartner = false;//是否为合伙人
+
+    private Integer agentLevel;//代理商级别
+
+    private BigDecimal discount;//代理商折扣
+
+    private Integer quantity;//数量
 
     public String getSpuName() {
         return spuName;
@@ -55,5 +67,37 @@ public class Product extends ComSku{
 
     public void setComSkuImages(List<ComSkuImage> comSkuImages) {
         this.comSkuImages = comSkuImages;
+    }
+
+    public Boolean getIsPartner() {
+        return isPartner;
+    }
+
+    public void setIsPartner(Boolean isPartner) {
+        this.isPartner = isPartner;
+    }
+
+    public Integer getAgentLevel() {
+        return agentLevel;
+    }
+
+    public void setAgentLevel(Integer agentLevel) {
+        this.agentLevel = agentLevel;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
     }
 }

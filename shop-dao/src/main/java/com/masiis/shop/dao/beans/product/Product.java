e852a708dd6ad858bb2316ba9ac6b2524bca2cb7
Tracy_jjh@163.com <jia13546550112>
2016-03-05 14:36:50 +0800

    规范代码

--- shop-dao/src/main/java/com/masiis/shop/dao/beans/product/Product.java
+++ shop-dao/src/main/java/com/masiis/shop/dao/beans/product/Product.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 /**
- * Created by ZhaoLiang on 2016/3/2.
+ * Created by JingHao on 2016/3/2.
  */
 public class Product extends ComSku {
 
--- shop-web/src/main/java/com/masiis/shop/web/platform/service/product/ProductService.java
+++ shop-web/src/main/java/com/masiis/shop/web/platform/service/product/ProductService.java
@@ -23,9 +23,10 @@ public class ProductService {
     private ProductMapper productMapper;
 
     /**
-     * 根据商品ID展示商品属性详情
-     * @param skuId
-     */
+      * @Author 贾晶豪
+      * @Date 2016/3/5 0005 下午 2:30
+      * 根据商品ID展示商品属性详情
+      */
     public Product getSkuDetails(String skuId) throws Exception{
         Product product = productMapper.getSkuDetailsBySkuId(skuId);
         List<ComSkuImage> skuImgList = productMapper.getSkuImgById(skuId);
@@ -39,8 +40,10 @@ public class ProductService {
         return product;
     }
     /**
-     * 代理商折扣，基础数据
-     */
+      * @Author 贾晶豪
+      * @Date 2016/3/5 0005 下午 2:30
+      * 代理商折扣，基础数据
+      */
     public String getDiscountByAgentLevel() throws Exception {
         String discountLevel = null;
         List<ComAgentLevel> comAgentLevels = productMapper.agentLevelDiscount();
@@ -49,5 +52,4 @@ public class ProductService {
         }
         return discountLevel;
     }
-
 }

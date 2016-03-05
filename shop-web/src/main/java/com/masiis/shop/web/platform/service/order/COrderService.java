3c929a5ec0724a93859df8dafec1941c8f4536cd
hanzengzhi <hzzh871225>
2016-03-05 14:16:01 +0800

    试用申请逻辑实现

--- shop-web/src/main/java/com/masiis/shop/web/platform/service/order/COrderService.java
+++ shop-web/src/main/java/com/masiis/shop/web/platform/service/order/COrderService.java
@@ -1,7 +1,28 @@
 package com.masiis.shop.web.platform.service.order;
 
+import com.masiis.shop.dao.po.PfUserTrial;
+import com.masiis.shop.web.platform.service.user.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
 /**
  * Created by ZhaoLiang on 2016/3/2.
  */
+@Service
+@Transactional
 public class COrderService {
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 试用申请
+     * @param pfUserTrial
+     */
+    public void trialApplyService(PfUserTrial pfUserTrial){
+        //更新试用用户信息
+        userService.updateUserTrial(pfUserTrial);
+    }
 }

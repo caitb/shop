a615c156b6420d9d2c8a3af5f38eadd7c750c8ca
mucf <18234036566@163.com>
2016-03-10 20:07:53 +0800

    上架

--- shop-web/src/main/java/com/masiis/shop/web/platform/controller/system/ProductListController.java
+++ shop-web/src/main/java/com/masiis/shop/web/platform/controller/system/ProductListController.java
@@ -2,9 +2,11 @@ package com.masiis.shop.web.platform.controller.system;
 
 import com.masiis.shop.common.util.PropertiesUtils;
 import com.masiis.shop.dao.beans.system.IndexComSku;
+import com.masiis.shop.dao.po.ComSpu;
 import com.masiis.shop.dao.po.ComUser;
 import com.masiis.shop.web.platform.service.product.ProductService;
 import com.masiis.shop.web.platform.service.system.IndexShowService;
+import com.masiis.shop.web.platform.service.system.SpuService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,7 +24,8 @@ import java.util.List;
 @RequestMapping("/productList")
 public class ProductListController {
 
-
+    @Resource
+    private SpuService spuService;
     @Resource
     private IndexShowService indexShowService;
 
@@ -36,23 +40,28 @@ public class ProductListController {
         //获取图片地址常量
         String value = PropertiesUtils.getStringValue("index_product_800_800_url");
         List<IndexComSku> indexComSk = indexShowService.findIndexComSku();
+        List<IndexComSku> Com =new ArrayList<IndexComSku>();
         for (IndexComSku indexComSku:indexComSk) {
-            //获取商品图片地址
-            String url = value + indexComSku.getImgUrl();
-            //重新封装商品图片地址
-            indexComSku.setImgUrl(url);
-            if(comUser!=null && comUser.getIsAgent()==1){
-                //确定代理权限
-                indexComSku.setIsPartner(true);
-                //显示优惠区间
-                indexComSku.setDiscountLevel(productService.getDiscountByAgentLevel(indexComSku.getComSku().getPriceRetail()));
-            }else{
-                indexComSku.setDiscountLevel("成为合伙人可查看");
+            ComSpu comSpu = spuService.getSpuById(indexComSku.getComSku().getSpuId());
+            if(comSpu.getIsSale()==1) {
+                //获取商品图片地址
+                String url = value + indexComSku.getImgUrl();
+                //重新封装商品图片地址
+                indexComSku.setImgUrl(url);
+                if (comUser != null && comUser.getIsAgent() == 1) {
+                    //确定代理权限
+                    indexComSku.setIsPartner(true);
+                    //显示优惠区间
+                    indexComSku.setDiscountLevel(productService.getDiscountByAgentLevel(indexComSku.getComSku().getPriceRetail()));
+                } else {
+                    indexComSku.setDiscountLevel("成为合伙人可查看");
+                }
+                Com.add(indexComSku);
             }
         }
         ModelAndView modelAndView = new ModelAndView();
         //封装展示商品信息集合
-        modelAndView.addObject("indexComSkus",indexComSk);
+        modelAndView.addObject("indexComSkus",Com);
         modelAndView.setViewName("platform/system/liebiao");
         return modelAndView;
     }

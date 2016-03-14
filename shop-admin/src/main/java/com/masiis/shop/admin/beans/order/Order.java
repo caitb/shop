6ed0d7cc8035abf0bde60f335543bca79b70d7b3
caitingbiao <yanbiao.cai@masiis.com>
2016-03-14 10:30:18 +0800

    commit

---
+++ shop-admin/src/main/java/com/masiis/shop/admin/beans/order/Order.java
@@ -0,0 +1,39 @@
+package com.masiis.shop.admin.beans.order;
+
+import com.masiis.shop.dao.po.ComUser;
+import com.masiis.shop.dao.po.PfBorder;
+import com.masiis.shop.dao.po.PfBorderConsignee;
+
+/**
+ * Created by cai_tb on 16/3/12.
+ */
+public class Order {
+
+    private PfBorder pfBorder;
+    private ComUser comUser;
+    private PfBorderConsignee pfBorderConsignee;
+
+    public PfBorder getPfBorder() {
+        return pfBorder;
+    }
+
+    public void setPfBorder(PfBorder pfBorder) {
+        this.pfBorder = pfBorder;
+    }
+
+    public ComUser getComUser() {
+        return comUser;
+    }
+
+    public void setComUser(ComUser comUser) {
+        this.comUser = comUser;
+    }
+
+    public PfBorderConsignee getPfBorderConsignee() {
+        return pfBorderConsignee;
+    }
+
+    public void setPfBorderConsignee(PfBorderConsignee pfBorderConsignee) {
+        this.pfBorderConsignee = pfBorderConsignee;
+    }
+}

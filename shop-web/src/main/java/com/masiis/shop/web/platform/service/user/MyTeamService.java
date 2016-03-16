daa216bf919fdd389a9ee4bdaa47e7fcf17dd0f6
caitb <caitb@139.com>
2016-03-16 19:24:04 +0800

    查看我的团队成员信心

--- shop-web/src/main/java/com/masiis/shop/web/platform/service/user/MyTeamService.java
+++ shop-web/src/main/java/com/masiis/shop/web/platform/service/user/MyTeamService.java
@@ -3,11 +3,9 @@ package com.masiis.shop.web.platform.service.user;
 import com.masiis.shop.dao.platform.product.ComAgentLevelMapper;
 import com.masiis.shop.dao.platform.product.ComSkuMapper;
 import com.masiis.shop.dao.platform.user.ComUserMapper;
+import com.masiis.shop.dao.platform.user.PfUserCertificateMapper;
 import com.masiis.shop.dao.platform.user.PfUserSkuMapper;
-import com.masiis.shop.dao.po.ComAgentLevel;
-import com.masiis.shop.dao.po.ComSku;
-import com.masiis.shop.dao.po.ComUser;
-import com.masiis.shop.dao.po.PfUserSku;
+import com.masiis.shop.dao.po.*;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -30,6 +28,8 @@ public class MyTeamService {
     private ComUserMapper comUserMapper;
     @Resource
     private ComAgentLevelMapper comAgentLevelMapper;
+    @Resource
+    private PfUserCertificateMapper pfUserCertificateMapper;
 
 
     /**
@@ -58,6 +58,13 @@ public class MyTeamService {
         return agentSkuMaps;
     }
 
+    /**
+     * 获取团队列表
+     * @param userSkuId
+     * @param skuId
+     * @param isCertificate
+     * @return
+     */
     public List<Map<String, Object>> findTeam(Integer userSkuId, Integer skuId, Integer isCertificate){
         PfUserSku pfUserSku = new PfUserSku();
         pfUserSku.setPid(userSkuId);
@@ -74,10 +81,33 @@ public class MyTeamService {
             Map<String, Object> teamMap = new HashMap<>();
             teamMap.put("comUserId", comUser.getId());
             teamMap.put("comUserName", comUser.getRealName());
+            teamMap.put("skuId", pus.getSkuId());
             teamMap.put("agentLevelId", comAgentLevel.getId());
             teamMap.put("agentLevelName", comAgentLevel.getName());
+            teamMap.put("code", pus.getCode());
         }
 
         return teamMaps;
+    }
+
+    public Map<String, Object> viewMember(Long comUserId, Integer skuId, Integer agentLevelId){
+        ComUser comUser = comUserMapper.selectByPrimaryKey(comUserId);
+        ComSku comSku = comSkuMapper.selectById(skuId);
+        ComAgentLevel comAgentLevel = comAgentLevelMapper.selectByPrimaryKey(agentLevelId);
+        PfUserCertificate pfUserCertificate = pfUserCertificateMapper.selectByUserId(comUserId);
+
+        Map<String, Object> memberMap = new HashMap<>();
+        memberMap.put("comUserId", comUser.getId());
+        memberMap.put("comUserName", comUser.getRealName());
+        memberMap.put("mobile", comUser.getMobile());
+        memberMap.put("weixin", comUser.getWxId());
+        memberMap.put("idCard", comUser.getIdCard());
+        memberMap.put("frontImg", comUser.getIdCardFrontUrl());
+        memberMap.put("backImg", comUser.getIdCardBackUrl());
+        memberMap.put("skuName", comSku.getName());
+        memberMap.put("agentLevelName", comAgentLevel.getName());
+        memberMap.put("certificateImg", pfUserCertificate.getImgUrl());
+
+        return memberMap;
     }
 }

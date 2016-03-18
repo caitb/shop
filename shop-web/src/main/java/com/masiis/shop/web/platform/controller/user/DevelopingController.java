3329852c67c9d1606df499749ef08d139c16ecb8
caitb <caitb@139.com>
2016-03-18 12:35:33 +0800

    JSSDK微信分享

--- shop-web/src/main/java/com/masiis/shop/web/platform/controller/user/DevelopingController.java
+++ shop-web/src/main/java/com/masiis/shop/web/platform/controller/user/DevelopingController.java
@@ -1,5 +1,8 @@
 package com.masiis.shop.web.platform.controller.user;
 
+import com.masiis.shop.web.platform.constants.WxConstants;
+import com.masiis.shop.web.platform.task.JsapiTicketTask;
+import com.masiis.shop.web.platform.utils.SpringRedisUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,6 +10,13 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * 发展合伙人
@@ -21,6 +31,85 @@ public class DevelopingController {
         ModelAndView mav = new ModelAndView("platform/user/developing");
 
         return mav;
+    }
+
+    @RequestMapping("/sharelink")
+    public ModelAndView shareLink(HttpServletRequest request, HttpServletResponse response, Long userId, Integer skuId){
+
+        ModelAndView mav = new ModelAndView("platform/user/sharePage");
+
+        String curUrl = request.getRequestURL().toString();
+        String jsapi_ticket = SpringRedisUtil.get("jsapi_ticket", String.class);
+        if(jsapi_ticket == null){
+            jsapi_ticket = new JsapiTicketTask().requestTicket();
+        }
+
+        Map<String, String> resultMap = sign(jsapi_ticket, curUrl);
+        resultMap.put("appId", WxConstants.APPID);
+        resultMap.put("shareLink", null);
+        //TODO
+
+        mav.addObject("shareMap", resultMap);
+
+        return mav;
+    }
+
+    public static Map<String, String> sign(String jsapi_ticket, String url) {
+        Map<String, String> ret = new HashMap<String, String>();
+        String nonce_str = create_nonce_str();
+        String timestamp = create_timestamp();
+        String string1;
+        String signature = "";
+
+        //注意这里参数名必须全部小写，且必须有序
+        string1 = "jsapi_ticket=" + jsapi_ticket +
+                "&noncestr=" + nonce_str +
+                "&timestamp=" + timestamp +
+                "&url=" + url;
+        System.out.println(string1);
+
+        try
+        {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(string1.getBytes("UTF-8"));
+            signature = byteToHex(crypt.digest());
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+
+        ret.put("url", url);
+        ret.put("jsapi_ticket", jsapi_ticket);
+        ret.put("nonceStr", nonce_str);
+        ret.put("timestamp", timestamp);
+        ret.put("signature", signature);
+
+        return ret;
+    }
+
+    private static String byteToHex(final byte[] hash) {
+        Formatter formatter = new Formatter();
+        for (byte b : hash)
+        {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
+    }
+
+    private static String create_nonce_str() {
+        return UUID.randomUUID().toString();
+    }
+
+    private static String create_timestamp() {
+        return Long.toString(System.currentTimeMillis() / 1000);
     }
 
 }

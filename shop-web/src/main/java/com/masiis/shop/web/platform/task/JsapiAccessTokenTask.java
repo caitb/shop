b59c0bff22c8c0f01163a3b8e0c038f63408b6e7
caitb <caitb@139.com>
2016-03-18 10:32:58 +0800

    JSSDK微信分享

---
+++ shop-web/src/main/java/com/masiis/shop/web/platform/task/JsapiAccessTokenTask.java
@@ -0,0 +1,51 @@
+package com.masiis.shop.web.platform.task;
+
+import com.alibaba.druid.support.json.JSONParser;
+import com.alibaba.druid.support.logging.Log;
+import com.alibaba.druid.support.logging.LogFactory;
+import com.masiis.shop.common.util.HttpClientUtils;
+import com.masiis.shop.web.platform.constants.WxConstants;
+import com.masiis.shop.web.platform.utils.SpringRedisUtil;
+
+import java.util.Map;
+
+/**
+ * 获取jsapi的令牌
+ * Created by cai_tb on 16/3/17.
+ */
+public class JsapiAccessTokenTask {
+
+    private final Log log = LogFactory.getLog(JsapiAccessTokenTask.class);
+
+    public String requestToken(){
+        String accessTokenUrl = null;
+        String jsonResult = null;
+
+        try {
+            accessTokenUrl = WxConstants.URL_JSSDK_ACCESS_TOKEN
+                                   + "?grant_type=" + WxConstants.JSSDK_GRANT_TYPE
+                                   + "&appid=" + WxConstants.APPID
+                                   + "&secret=" + WxConstants.APPSECRET;
+            jsonResult     = HttpClientUtils.httpGet(accessTokenUrl);
+            Map<String, Object> resultMap = new JSONParser(jsonResult).parseMap();
+
+            if (resultMap.get("access_token") != null){
+                SpringRedisUtil.saveEx("jsapi_access_tocken", resultMap.get("access_token"), Long.parseLong((String)resultMap.get("access_token")));//7200=2小时
+            }else{
+                throw new RuntimeException("获取jsapi的令牌出错!");
+            }
+
+            return (String)resultMap.get("access_token");
+        } catch (Exception e){
+            e.printStackTrace();
+            log.error("获取jsapi的令牌出错:[accessTokenUrl=" + accessTokenUrl + "][jsonResult=" + jsonResult + "]");
+            return jsonResult;
+        }
+    }
+
+    public static void main(String[] args){
+        JsapiAccessTokenTask j = new JsapiAccessTokenTask();
+        String res = j.requestToken();
+        System.out.println("result: " + res);
+    }
+}

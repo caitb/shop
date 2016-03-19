2d1b13d12073f535ac8ed0075ac7f9c5d45a5b8b
caitb <caitb@139.com>
2016-03-19 16:37:59 +0800

    后台添加商品

--- shop-admin/src/main/java/com/masiis/shop/admin/controller/ueditor/UEditorController.java
+++ shop-admin/src/main/java/com/masiis/shop/admin/controller/ueditor/UEditorController.java
@@ -31,19 +31,19 @@ public class UEditorController {
         String configJson = new ActionEnter(request, rootPath).exec();
 
         /*为了在富文本选择图片时能够马上显示,加入一下代码直接上传到OSS图片服务器*/
-        String action = request.getParameter("action");
-        if("uploadimage".equals(action)){
-            /* 上传商品详情图 */
-            String realPath = request.getServletContext().getRealPath("/");
-            File detailDir = new File(realPath + "static/product/detail_img");
-            if(detailDir.exists() && detailDir.listFiles().length > 0){
-                File[] files = detailDir.listFiles();
-                for(File f : files){
-                    OSSObjectUtils.uploadFile("mmshop", f, "static/product/detail_img/");
-                    f.delete();
-                }
-            }
-        }
+//        String action = request.getParameter("action");
+//        if("uploadimage".equals(action)){
+//            /* 上传商品详情图 */
+//            String realPath = request.getServletContext().getRealPath("/");
+//            File detailDir = new File(realPath + "static/product/detail_img");
+//            if(detailDir.exists() && detailDir.listFiles().length > 0){
+//                File[] files = detailDir.listFiles();
+//                for(File f : files){
+//                    OSSObjectUtils.uploadFile("mmshop", f, "static/product/detail_img/");
+//                    f.delete();
+//                }
+//            }
+//        }
 
         return configJson;
     }

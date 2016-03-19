277d5800fd571e92b2a814dd61864ea20d5f7a0e
zhaoliang <370787497@qq.com>
2016-03-19 18:48:45 +0800

    update bug

--- shop-web/src/main/java/com/masiis/shop/web/platform/service/order/BOrderService.java
+++ shop-web/src/main/java/com/masiis/shop/web/platform/service/order/BOrderService.java
@@ -206,6 +206,7 @@ public class BOrderService {
                 defaultUserSkuStock.setSkuId(pfBorderItem.getSkuId());
                 defaultUserSkuStock.setStock(0);
                 defaultUserSkuStock.setFrozenStock(0);
+                defaultUserSkuStock.setVersion(0);
                 pfUserSkuStockMapper.insert(defaultUserSkuStock);
             }
         }

58bf617af64e4820cdff58459beaf9d9746a043f
luozhihui <767508395@qq.com>
2016-03-22 19:18:25 +0800

    修改我的资产弹框样式

--- shop-web/src/main/webapp/static/js/extract_apply.js
+++ shop-web/src/main/webapp/static/js/extract_apply.js
@@ -12,7 +12,8 @@ $(function(){
         }
         // 可以提现
         $("#ex_money").html(money + " 元");
-        $(".back").css("display", "-webkit-box");
+        $(".back").show();
+        $(".back_que").css("display", "-webkit-box");
     });
 
     $("#exBtnOk").on("click", function(){
@@ -38,6 +39,7 @@ $(function(){
 
     $("#exBtnCancel").on("click", function(){
         $(".back").css("display", "none");
+        $(".back_que").css("display", "none");
     });
 });
 

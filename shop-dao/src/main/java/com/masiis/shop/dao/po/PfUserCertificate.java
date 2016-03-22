1ff225aec6ad3c6365afc80d7369223b07f2215a
caitb <caitb@139.com>
2016-03-22 19:51:01 +0800

    订单管理

--- shop-dao/src/main/java/com/masiis/shop/dao/po/PfUserCertificate.java
+++ shop-dao/src/main/java/com/masiis/shop/dao/po/PfUserCertificate.java
@@ -81,102 +81,134 @@ public class PfUserCertificate {
 
     private String reason;//拒绝理由
 
+    private String poster;
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public Date getCreateTime() {
         return createTime;
     }
+
     public void setCreateTime(Date createTime) {
         this.createTime = createTime;
     }
+
     public String getCode() {
         return code;
     }
+
     public void setCode(String code) {
-        this.code = code == null ? null : code.trim();
+        this.code = code;
     }
+
     public Integer getPfUserSkuId() {
         return pfUserSkuId;
     }
+
     public void setPfUserSkuId(Integer pfUserSkuId) {
         this.pfUserSkuId = pfUserSkuId;
     }
+
     public Long getUserId() {
         return userId;
     }
+
     public void setUserId(Long userId) {
         this.userId = userId;
     }
+
     public Integer getSpuId() {
         return spuId;
     }
+
     public void setSpuId(Integer spuId) {
         this.spuId = spuId;
     }
+
     public Integer getSkuId() {
         return skuId;
     }
+
     public void setSkuId(Integer skuId) {
         this.skuId = skuId;
     }
+
     public String getIdCard() {
         return idCard;
     }
+
     public void setIdCard(String idCard) {
-        this.idCard = idCard == null ? null : idCard.trim();
+        this.idCard = idCard;
     }
+
     public String getMobile() {
         return mobile;
     }
+
     public void setMobile(String mobile) {
-        this.mobile = mobile == null ? null : mobile.trim();
+        this.mobile = mobile;
     }
+
     public String getWxId() {
         return wxId;
     }
+
     public void setWxId(String wxId) {
-        this.wxId = wxId == null ? null : wxId.trim();
+        this.wxId = wxId;
     }
+
     public Date getBeginTime() {
         return beginTime;
     }
+
     public void setBeginTime(Date beginTime) {
         this.beginTime = beginTime;
     }
+
     public Date getEndTime() {
         return endTime;
     }
+
     public void setEndTime(Date endTime) {
         this.endTime = endTime;
     }
+
     public Integer getAgentLevelId() {
         return agentLevelId;
     }
+
     public void setAgentLevelId(Integer agentLevelId) {
         this.agentLevelId = agentLevelId;
     }
+
     public String getImgUrl() {
         return imgUrl;
     }
+
     public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl == null ? null : imgUrl.trim();
+        this.imgUrl = imgUrl;
     }
+
     public Integer getStatus() {
         return status;
     }
-    public Integer setStatus(Integer status) {
+
+    public void setStatus(Integer status) {
         this.status = status;
-        return status;
     }
+
     public String getRemark() {
         return remark;
     }
+
     public void setRemark(String remark) {
-        this.remark = remark == null ? null : remark.trim();
+        this.remark = remark;
     }
 
     public ComSku getComSku() {
@@ -198,6 +230,7 @@ public class PfUserCertificate {
     public String getSjName() {
         return sjName;
     }
+
     public void setSjName(String sjName) {
         this.sjName = sjName;
     }
@@ -208,5 +241,13 @@ public class PfUserCertificate {
 
     public void setReason(String reason) {
         this.reason = reason;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
     }
 }

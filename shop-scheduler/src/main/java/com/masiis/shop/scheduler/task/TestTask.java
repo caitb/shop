0fcb2fc96a38f617ba89396d40e831e27e51085b
luozhihui <767508395@qq.com>
2016-03-22 14:42:37 +0800

    修改我的资产bug

--- shop-scheduler/src/main/java/com/masiis/shop/scheduler/task/TestTask.java
+++ shop-scheduler/src/main/java/com/masiis/shop/scheduler/task/TestTask.java
@@ -2,9 +2,14 @@ package com.masiis.shop.scheduler.task;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 /**
  * Created by lzh on 2016/3/22.
  */
 @Component
 public class TestTask {
+    public void testJob(){
+        System.out.println(new Date());
+    }
 }

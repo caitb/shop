7e6e5f47e0e50dec733e7a087a8ae6cd3a6cbb0c
Tracy_jjh@163.com <jia13546550112>
2016-03-23 14:34:23 +0800

    授权书相关bug修改

--- shop-web/src/main/java/com/masiis/shop/web/platform/controller/user/UserCertificateController.java
+++ shop-web/src/main/java/com/masiis/shop/web/platform/controller/user/UserCertificateController.java
@@ -33,7 +33,6 @@ import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 /**
  * UserCertificateController
@@ -333,18 +332,18 @@ public class UserCertificateController {
      * Jing Hao
      * param :user_sku_id
      */
-    @RequestMapping(value = "/add/{pfuId}")
-    public ModelAndView addCertificate(HttpServletRequest request, HttpServletResponse response,
-                                       @PathVariable("pfuId") Integer pfuId) throws Exception {
-        ModelAndView mav = new ModelAndView("/platform/user/cready");
+    @RequestMapping(value = "/updatect.do")
+    @ResponseBody
+    public String addCertificate(HttpServletRequest request, HttpServletResponse response,
+                                 @RequestParam(value = "pfuId", required = true) Integer pfuId) throws Exception {
+        JSONObject object = new JSONObject();
         try {
-            Map<String, Object> obj = userCertificateService.receiveCertificate(pfuId);
-            if (obj.get("success") == true) {
-                mav.addObject("skuName", obj.get("skuName"));
-            }
+            userCertificateService.receiveCertificate(pfuId);
+            object.put("isError", false);
         } catch (Exception ex) {
+            object.put("isError", true);
             throw new BusinessException("领取证书失败!");
         }
-        return mav;
+        return object.toJSONString();
     }
 }

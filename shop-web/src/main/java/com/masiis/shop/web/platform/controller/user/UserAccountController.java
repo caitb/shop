f1951152d02263e15483732cf54ce7dedf4dc0c4
wangbingjian <bingjian.wang@masiis.com>
2016-03-24 17:14:16 +0800

    edit

--- shop-web/src/main/java/com/masiis/shop/web/platform/controller/user/UserAccountController.java
+++ shop-web/src/main/java/com/masiis/shop/web/platform/controller/user/UserAccountController.java
@@ -144,21 +144,4 @@ public class UserAccountController extends BaseController{
 //        List<PfUserBill> userBills = pfUserBillService.findByUserIdLimtPage(user.getId(),year+month,1,10);
 //
 //    }
-
-    public static void main(String[] args) {
-        Calendar cal = Calendar.getInstance();
-        int day = cal.get(Calendar.DATE);
-        int month = cal.get(Calendar.MONTH) + 1;
-        int year = cal.get(Calendar.YEAR);
-        int dow = cal.get(Calendar.DAY_OF_WEEK);
-        int dom = cal.get(Calendar.DAY_OF_MONTH);
-        int doy = cal.get(Calendar.DAY_OF_YEAR);
-        System.out.println("Current Date: " + cal.getTime());
-        System.out.println("Day: " + day);
-        System.out.println("Month: " + month);
-        System.out.println("Year: " + year);
-        System.out.println("Day of Week: " + dow);
-        System.out.println("Day of Month: " + dom);
-        System.out.println("Day of Year: " + doy);
-    }
 }

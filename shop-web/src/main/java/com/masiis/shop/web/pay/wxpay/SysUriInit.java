2d10a8798b15e69c98f670f6b21fcf29545aeb83
luozhihui <767508395@qq.com>
2016-03-29 15:10:12 +0800

    修改目录

--- shop-web/src/main/java/com/masiis/shop/web/pay/wxpay/SysUriInit.java
+++ shop-web/src/main/java/com/masiis/shop/web/pay/wxpay/SysUriInit.java
@@ -1,4 +1,4 @@
-package com.masiis.shop.web.System.init;
+package com.masiis.shop.web.pay.wxpay;
 
 import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.context.ApplicationContext;

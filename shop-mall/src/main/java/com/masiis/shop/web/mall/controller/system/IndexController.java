8572eec366855c24503813919a7c32be7af11b5f
mucf <18234036566@163.com>
2016-04-10 18:48:14 +0800

    首页

--- shop-mall/src/main/java/com/masiis/shop/web/mall/controller/system/IndexController.java
+++ shop-mall/src/main/java/com/masiis/shop/web/mall/controller/system/IndexController.java
@@ -44,7 +44,7 @@ public class IndexController extends BaseController {
             user = userService.getUserById(1l);
             req.getSession().setAttribute("comUser", user);
         }
-//        SfUserRelation sfUserRelation = sfOrderManageService.findSfUserRelationByUserId(user.getId());
+        userService.getShareUser(user.getId(),userPid);//分销关系
 //        ComUser pUser = userService.getUserById(sfUserRelation.getUserPid());
         ComUser pUser = new ComUser();
         SfShop sfShop = sfShopService.getSfShopById(shopId);

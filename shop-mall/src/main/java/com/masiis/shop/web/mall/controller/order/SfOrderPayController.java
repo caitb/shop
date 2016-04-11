e1bb81ecb4859cf9f806ef4e7bcf75dad91f076b
hanzengzhi <516490578@qq.com>
2016-04-11 18:27:11 +0800

    修改微信回调地址

--- shop-mall/src/main/java/com/masiis/shop/web/mall/controller/order/SfOrderPayController.java
+++ shop-mall/src/main/java/com/masiis/shop/web/mall/controller/order/SfOrderPayController.java
@@ -7,7 +7,6 @@ package com.masiis.shop.web.mall.controller.order;
 import com.alibaba.fastjson.JSONObject;
 import com.masiis.shop.web.mall.beans.pay.wxpay.WxPaySysParamReq;
 import com.masiis.shop.web.mall.service.order.SfOrderPayService;
-import com.masiis.shop.web.mall.service.order.SfOrderPaymentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;

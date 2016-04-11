117e0d92f013c3b3ee2a5984a09e9661ac174ad6
luozhihui <767508395@qq.com>
2016-04-11 10:50:45 +0800

    修改小铺用户提现和账户操作记录的service代码

--- shop-mall/src/main/java/com/masiis/shop/web/mall/service/user/SfUserAccountService.java
+++ shop-mall/src/main/java/com/masiis/shop/web/mall/service/user/SfUserAccountService.java
@@ -1,9 +1,13 @@
 package com.masiis.shop.web.mall.service.user;
 
 import com.masiis.shop.dao.mall.user.SfUserAccountMapper;
+import com.masiis.shop.dao.po.ComUser;
 import com.masiis.shop.dao.po.SfUserAccount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * Created by wangbingjian on 2016/4/8.
@@ -23,4 +27,20 @@ public class SfUserAccountService {
         return userAccountMapper.selectByUserId(userId);
     }
 
+    /**
+     * 根据ComUser创建小铺端用户账户对象
+     *
+     * @param user
+     */
+    public void createSfAccountByUser(ComUser user) {
+        SfUserAccount account = new SfUserAccount();
+
+        account.setCreateTime(new Date());
+        account.setExtractableFee(new BigDecimal(0));
+        account.setCountingFee(new BigDecimal(0));
+        account.setUserId(user.getId());
+        account.setVersion(0L);
+
+        userAccountMapper.insert(account);
+    }
 }

475a5a3f38fb20a9014676a316469dfa448ae0d5
luozhihui <767508395@qq.com>
2016-04-12 15:50:03 +0800

    小铺订单确认收货算钱

--- shop-mall/src/main/java/com/masiis/shop/web/mall/service/user/SfUserAccountService.java
+++ shop-mall/src/main/java/com/masiis/shop/web/mall/service/user/SfUserAccountService.java
@@ -1,11 +1,19 @@
 package com.masiis.shop.web.mall.service.user;
 
+import com.masiis.shop.common.enums.mall.SfOrderStatusEnum;
+import com.masiis.shop.common.exceptions.BusinessException;
+import com.masiis.shop.dao.mall.order.SfOrderMapper;
 import com.masiis.shop.dao.mall.user.SfUserAccountMapper;
 import com.masiis.shop.dao.po.ComUser;
+import com.masiis.shop.dao.po.SfOrder;
 import com.masiis.shop.dao.po.SfUserAccount;
+import com.masiis.shop.web.mall.service.order.SfOrderService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -14,9 +22,12 @@ import java.util.Date;
  */
 @Service
 public class SfUserAccountService {
+    private Logger log = Logger.getLogger(this.getClass());
 
-    @Autowired
+    @Resource
     private SfUserAccountMapper userAccountMapper;
+    @Resource
+    private SfOrderMapper orderMapper;
 
     /**
      * 根据用户id查询分销用户账户表
@@ -42,5 +53,34 @@ public class SfUserAccountService {
         account.setVersion(0L);
 
         userAccountMapper.insert(account);
+    }
+
+    /**
+     * 结算小铺订单
+     *
+     * @param order
+     */
+    @Transactional
+    public void countingSfOrder(SfOrder order){
+        try{
+            if(order == null || order.getId() == null){
+                log.error("传入订单对象为null");
+                throw new BusinessException();
+            }
+            order = orderMapper.selectByPrimaryKey(order.getId());
+            if(order.getOrderType() != 0){
+                log.error("订单类型不匹配");
+                throw new BusinessException();
+            }
+            if(order.getOrderStatus().intValue() != SfOrderStatusEnum.ORDER_SHIPED.getCode().intValue()){
+                log.error("订单状态不匹配,订单不是" + SfOrderStatusEnum.ORDER_SHIPED.getDesc() + "状态");
+                throw new BusinessException("订单状态不匹配,订单不是"
+                        + SfOrderStatusEnum.ORDER_SHIPED.getDesc() + "状态");
+            }
+
+
+        } catch (Exception e) {
+
+        }
     }
 }

263abe7970a11016da6bbb9c9786ff6089f88e6a
wangbingjian <bingjian.wang@masiis.com>
2016-04-12 16:48:38 +0800

    小铺开发

--- shop-dao/src/main/java/com/masiis/shop/dao/mall/user/SfUserShopViewExtendMapper.java
+++ shop-dao/src/main/java/com/masiis/shop/dao/mall/user/SfUserShopViewExtendMapper.java
@@ -5,10 +5,13 @@ package com.masiis.shop.dao.mall.user;
 
 
 import com.masiis.shop.dao.po.SfUserShopView;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public interface SfUserShopViewExtendMapper {
-    List<SfUserShopView> selectViewedShopByUserId(Long userId);
+    List<SfUserShopView> selectViewedShopByUserId(@Param(value = "userId") Long userId);
+
+    Integer selectCountByUserId(@Param(value = "userId") Long userId);
 }

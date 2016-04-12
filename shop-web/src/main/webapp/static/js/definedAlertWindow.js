099f3152db59d73074074ae72f75214110e4206c
lijialin <gkhk_gkhk@163.com>
2016-04-12 15:50:10 +0800

    add

--- shop-web/src/main/webapp/static/js/definedAlertWindow.js
+++ shop-web/src/main/webapp/static/js/definedAlertWindow.js
@@ -9,20 +9,20 @@
   //生成Html
   var generateHtml = function (msg) {
     var _html = "";
-    _html += '<div class="alert" style="display: none"><h1><img src="http://'+ window.location.host+'/static/images/alert.png" alt="">提示</h1>';
-    _html += '<h2>' + msg + '</h2></div>';
+    _html += '<div class="alert" style="display: none"><h1>' + msg + '</h1>';
+    _html += '<h2><button>确定</button></h2></div>';
       var _back="";
       _back +='<div class="layer"></div>'
     $("body").append(_html);
       $("body").append(_back);
   }
   var hideWindow = function(event){
-        $('.alert').fadeIn("slow").delay(2000).fadeOut("slow", function(){
+        $('.alert').fadeIn("slow").delay(2000)/*.fadeOut("slow", function(){
           $('.alert').remove();
-        });
-      $(".layer").fadeIn("slow").delay(2000).fadeOut("slow", function(){
+        });*/
+      $(".layer").fadeIn("slow")/*.delay(2000).fadeOut("slow", function(){
           $('.layer').remove();
-      });
+      });*/
       var event=event||event.window;
       event.stopPropagation();
       }

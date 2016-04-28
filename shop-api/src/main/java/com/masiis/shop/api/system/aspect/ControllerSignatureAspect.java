06a724db7c2537ad7ea0036f8ffa41f003d60275
luozhihui <767508395@qq.com>
2016-04-28 21:13:57 +0800

    shop-api增加自定义aop校验签名

--- shop-api/src/main/java/com/masiis/shop/api/system/aspect/ControllerSignatureAspect.java
+++ shop-api/src/main/java/com/masiis/shop/api/system/aspect/ControllerSignatureAspect.java
@@ -1,11 +1,17 @@
 package com.masiis.shop.api.system.aspect;
 
+import com.alibaba.fastjson.JSONObject;
+import com.masiis.shop.api.bean.base.BaseReq;
+import com.masiis.shop.api.bean.base.BaseRes;
+import com.masiis.shop.api.bean.system.LoginByPhoneReq;
 import com.masiis.shop.api.constants.SignValid;
+import com.masiis.shop.api.constants.SysResCodeCons;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
@@ -21,20 +27,52 @@ import java.io.InputStream;
 @Aspect
 @Component
 public class ControllerSignatureAspect {
+    private Logger log = Logger.getLogger(this.getClass());
 
-    @Before("within(com.masiis.shop.api.controller..*) && @annotation(rl)")
-    public void signatureValid(JoinPoint jp, SignValid rl) {
+    @Around("within(com.masiis.shop.api.controller..*) && @annotation(rl)")
+    public Object signatureValid(ProceedingJoinPoint point, SignValid rl) throws Throwable {
         Class clazz = rl.paramType();
-        rl.superClass().getSuperclass();
-        Object[] parames = jp.getArgs();//获取目标方法体参数
-        System.out.println("aspect......");
-        for(Object obj:parames){
-            if(obj instanceof HttpServletRequest){
-                HttpServletRequest request = (HttpServletRequest) obj;
-                String res = getRequestBody(request);
-                System.out.println("aspect:" + res);
+
+        Object[] parames = point.getArgs();//获取目标方法体参数
+        System.out.println("......");
+        Object req = null;
+        try {
+            for (Object obj : parames) {
+                if (obj instanceof HttpServletRequest) {
+                    HttpServletRequest request = (HttpServletRequest) obj;
+                    String res = getRequestBody(request);
+                    System.out.println("aspect:" + res);
+                    req = JSONObject.parseObject(res, clazz);
+                }
             }
+            for(int i = 0; i < parames.length; i++){
+                if (parames[i].getClass() == clazz) {
+                    parames[i] = req;
+                }
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            BaseRes errRes = new BaseRes();
+            setResUnknown(errRes);
+            return errRes;
         }
+
+        log.info("进入controller......");
+        // 进入controller
+        Object res = point.proceed(parames);
+
+        log.info("离开controller......");
+        // 自动校验返回码
+        BaseRes res1 = (BaseRes) res;
+        if(res1 == null || StringUtils.isBlank(res1.getResCode())){
+            setResUnknown(res1);
+        }
+        return res;
+    }
+
+    private void setResUnknown(BaseRes errRes) {
+        errRes.setResCode(SysResCodeCons.RES_CODE_NOT_KNOWN);
+        errRes.setResMsg(SysResCodeCons.RES_CODE_NOT_KNOWN_MSG);
     }
 
     private String getRequestBody(HttpServletRequest request) {
@@ -50,7 +88,7 @@ public class ControllerSignatureAspect {
                 os.write(buffer, 0, len);
             }
             res = new String(os.toByteArray(), "UTF-8");
-            if (is != null) {
+            if(is != null){
                 is.close();
             }
             os.close();

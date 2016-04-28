72a2a1ac99b23adfc50b621042221c8f453a48ad
luozhihui <767508395@qq.com>
2016-04-28 16:40:40 +0800

    增加自定义签名校验aop，暂时提交

---
+++ shop-api/src/main/java/com/masiis/shop/api/controller/user/IndexController.java
@@ -0,0 +1,15 @@
+package com.masiis.shop.api.controller.user;
+
+import com.masiis.shop.api.controller.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Date 2016/4/28
+ * @Auther lzh
+ */
+@Controller
+@RequestMapping("/")
+public class IndexController extends BaseController {
+
+}

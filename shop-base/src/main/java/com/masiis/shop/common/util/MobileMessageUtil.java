ecda16c8fd962fafd546b142d3ab18f8e199ab34
wangbingjian <bingjian.wang@masiis.com>
2016-05-05 10:14:50 +0800

    发送短信修改

--- shop-base/src/main/java/com/masiis/shop/common/util/MobileMessageUtil.java
+++ shop-base/src/main/java/com/masiis/shop/common/util/MobileMessageUtil.java
@@ -2,53 +2,67 @@ package com.masiis.shop.common.util;
 
 import com.masiis.shop.common.constant.SMSConstants;
 
-import java.util.Random;
+import java.math.BigDecimal;
 
 /**
  * MobileMessageUtil
  *
- * @author ZhaoLiang
+ * @author wbj
  * @date 2016/3/9
  */
 public class MobileMessageUtil {
 
     private static String sign = "_SMS";
 
+    /**
+     * 短信結束語
+     */
+    private static String EVENING_MESSAGE = "";
+    /**
+     * 系统标识
+     */
+    private static String fromIdentify = "";
 
+    private String[] content;
+
+    private MobileMessageUtil(String fromIdentify){
+        this.fromIdentify = fromIdentify;
+        if ("B".equals(this.fromIdentify)){
+            EVENING_MESSAGE = "关注麦链合伙人公众账号，查看最新状态";
+        }
+        if ("C".equals(this.fromIdentify)){
+            EVENING_MESSAGE = "关注麦链商城众账号，查看最新状态";
+        }
+    }
 
     /**
-     * 发送验证码
+     * 单例模式初始化  MobileMessageUtil
+     * @param fromIdentify
+     * @return
+     */
+    public static final MobileMessageUtil getInitialization(String fromIdentify) {
+        MobileMessageUtil mobileMessageUtil = new MobileMessageUtil(fromIdentify);
+        return mobileMessageUtil;
+    }
+
+    /**
+     * 短信验证码
      * @param phone
      * @param code      验证码
      * @param minute    有效分钟数
      * @return
      */
-    public static boolean VerificationCode(String phone, String code, String minute){
-        String[] content = new String[2];
+    public boolean verificationCode(String phone, String code, String minute){
+        content = new String[3];
         content[0] = code;
         content[1] = minute;
+        content[2] = EVENING_MESSAGE;
         String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.VERIFICATION_CODE, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
         }
         return true;
     }
-
-//    /**
-//     * 支付短信
-//     * @param phone             手机号码
-//     * @param skuName           商品名称
-//     * @param agentLevelName    代理等级名称
-//     * @return
-//     */
-//    public static boolean sendMessageForToPay(String phone, String skuName, String agentLevelName) {
-//        String[] content = new String[]{skuName,agentLevelName};
-//        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.TOPAY_TEMPLETE_ID, content);
-//        if (!"000000".equals(smsRes[0])) {
-//            return false;
-//        }
-//        return true;
-//    }
 
     /**
      * 合伙人申请成功提示
@@ -58,10 +72,11 @@ public class MobileMessageUtil {
      * @param agentLevelName    合伙人等级
      * @return
      */
-    public static boolean partnerApplicationSuccess(String phone, String skuName, String agentLevelName){
-        String[] content = new String[2];
+    public boolean partnerApplicationSuccess(String phone, String skuName, String agentLevelName){
+        content = new String[3];
         content[0] = skuName;
         content[1] = agentLevelName;
+        content[2] = EVENING_MESSAGE;
         String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.PARTNER_APPLICATION_SUCCESS, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
@@ -76,9 +91,10 @@ public class MobileMessageUtil {
      * @param days      审核工作日
      * @return
      */
-    public static boolean verifiedSubmitRemind(String phone, String days){
-        String[] content = new String[1];
+    public boolean verifiedSubmitRemind(String phone, String days){
+        content = new String[2];
         content[0] = days;
+        content[1] = EVENING_MESSAGE;
         String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.VERIFIED_SUBMIT_REMIND, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
@@ -89,13 +105,19 @@ public class MobileMessageUtil {
     /**
      * 实名认证审核结果
      * @param phone
-     * @param approved  true:通过审核  false:未通过审核
+     * @param flag  是否通过
      * @return
      */
-    public static boolean verifiedComplete(String phone, boolean approved){
-        String[] content = new String[2];
-        content[0] = approved==true?"通过审核":"未通过审核";
-        content[1] = approved==true?"继续申请合伙人":"重新提交实名认证";
+    public boolean certificationVerifyResult(String phone, boolean flag){
+        content = new String[3];
+        if (flag){
+            content[0] = "通过审核";
+            content[1] = "继续申请合伙人";
+        }else {
+            content[0] = "未通过审核";
+            content[1] = "重新提交实名认证";
+        }
+        content[2] = EVENING_MESSAGE;
         String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.VERIFIED_COMPLETE, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
@@ -111,11 +133,12 @@ public class MobileMessageUtil {
      * @param shipCode      快递单号
      * @return
      */
-    public static boolean goodsOrderShipRemind(String phone, String orderCode, String shipName, String shipCode){
-        String[] content = new String[3];
+    public boolean goodsOrderShipRemind(String phone, String orderCode, String shipName, String shipCode){
+        content = new String[4];
         content[0] = orderCode;
         content[1] = shipName;
         content[2] = shipCode;
+        content[3] = EVENING_MESSAGE;
         String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.ORDER_SHIP_REMIND, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
@@ -128,21 +151,10 @@ public class MobileMessageUtil {
      * @param phone
      * @return
      */
-    public static boolean haveNewLowerOrder(String phone){
-        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.NEW_LOWER_ORDER, null);
-        if (!"000000".equals(smsRes[0])) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * 发货订单
-     * @param phone
-     * @return
-     */
-    public static boolean shipOrder(String phone){
-        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.SHIP_ORDER, null);
+    public boolean haveNewLowerOrder(String phone){
+        content = new String[1];
+        content[0] = EVENING_MESSAGE;
+        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.NEW_LOWER_ORDER, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
         }
@@ -155,9 +167,10 @@ public class MobileMessageUtil {
      * @param days     工作日天数
      * @return
      */
-    public static boolean withdrawRequestVerifyAgent(String phone, String days){
-        String[] content = new String[1];
+    public boolean withdrawRequestVerifyAgent(String phone, String days){
+        content = new String[2];
         content[0] = days;
+        content[1] = EVENING_MESSAGE;
         String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.WITHDRAW_REQUEST_VERIFY_AGENT, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
@@ -166,59 +179,35 @@ public class MobileMessageUtil {
     }
 
     /**
-     * 小铺提现申请审核
-     * @param phone
-     * @param days     工作日天数
-     * @return
-     */
-    public static boolean withdrawRequestVerifyCustomer(String phone, String days){
-        String[] content = new String[1];
-        content[0] = days;
-        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.WITHDRAW_REQUEST_VERIFY_CUSTOMER, content);
-        if (!"000000".equals(smsRes[0])) {
-            return false;
-        }
-        return true;
-    }
-    /**
-     * 消费者提现审核通过
-     * @param phone
-     * @param days      工作日天数
-     * @param theWay    打款方式{1:微信 2:支付宝 3:银行卡}
-     * @return
-     */
-    public static boolean withdrawVerifyApproveCustomer(String phone, String days, Integer theWay){
-        String[] content = new String[]{days,theWay==1?"微信":theWay==2?"支付宝":theWay==3?"银行卡":""};
-        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.WITHDRAW_VERIFY_APPROVE_CUSTOMER, content);
-        if (!"000000".equals(smsRes[0])) {
-            return false;
-        }
-        return true;
-    }
-    /**
      * 代理提现审核通过
      * @param phone
      * @param days      工作日天数
      * @param theWay    打款方式{1:微信 2:支付宝 3:银行卡}
      * @return
      */
-    public static boolean withdrawVerifyApproveAgent(String phone, String days, Integer theWay){
-        String[] content = new String[]{days,theWay==1?"微信":theWay==2?"支付宝":theWay==3?"银行卡":""};
+    public boolean withdrawVerifyApproveAgent(String phone, String days, Integer theWay){
+        content = new String[3];
+        content[0] = days;
+        content[1] = theWay==1?"微信":theWay==2?"支付宝":theWay==3?"银行卡":"";
+        content[2] = EVENING_MESSAGE;
         String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.WITHDRAW_VERIFY_APPROVE_AGENT, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
         }
         return true;
     }
+
     /**
-     * 提现申请审核拒绝
+     * 提现申请审核拒绝-B端
      * @param phone
      * @param reason    拒绝原因
      * @return
      */
-    public static boolean withdrawVerifyRefuse(String phone, String reason){
-        String[] content = new String[]{reason};
-        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.WITHDRAW_VERIFY_REFUSE, content);
+    public boolean withdrawVerifyRefuseAgent(String phone, String reason){
+        content = new String[2];
+        content[0] = reason;
+        content[1] = EVENING_MESSAGE;
+        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.WITHDRAW_VERIFY_REFUSE_AGENT, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
         }
@@ -226,42 +215,24 @@ public class MobileMessageUtil {
     }
 
     /**
-     * 提现申请审核拒绝-c
+     * 补货成功提醒
      * @param phone
-     * @param reason    拒绝原因
+     * @param sendType  拿货类型
+     * @param quantity  库存数量
      * @return
      */
-    public static boolean withdrawVerifyRefuse_C(String phone, String reason){
-        String[] content = new String[]{reason};
-        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.WITHDRAW_VERIFY_REFUSE_C, content);
-        if (!"000000".equals(smsRes[0])) {
-            return false;
+    public boolean addStockSuccess(String phone, Integer sendType, String quantity){
+        content = new String[3];
+        if (1 == sendType){
+            content[0] = "平台代发";
+            content[1] = "在线库存已更新;" + quantity;
         }
-        return true;
-    }
-
-    /**
-     * 补货-平台代发
-     * @param phone
-     * @param quantity   补货数量
-     * @return
-     */
-    public static boolean addStockByPlatform(String phone, String quantity){
-        String[] content = new String[]{quantity};
-        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.ADD_STOCK_PLATFORM, content);
-        if (!"000000".equals(smsRes[0])) {
-            return false;
+        if (2 == sendType){
+            content[0] = "自己发货";
+            content[1] = "我们将尽快发货;" + quantity;
         }
-        return true;
-    }
-
-    /**
-     * 补货-自己发货
-     * @param phone
-     * @return
-     */
-    public static boolean addStockByUserself(String phone){
-        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.ADD_STOCK_SELF, null);
+        content[2] = EVENING_MESSAGE;
+        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.ADD_STOCK_SUCCESS, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
         }
@@ -274,9 +245,10 @@ public class MobileMessageUtil {
      * @param orderCode   订单编号
      * @return
      */
-    public static boolean joinQueueOrder(String phone, String orderCode){
-        String[] content = new String[1];
+    public boolean joinQueueOrder(String phone, String orderCode){
+        content = new String[2];
         content[0] = orderCode;
+        content[1] = EVENING_MESSAGE;
         String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.JOIN_QUEUE_ORDER, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
@@ -285,35 +257,17 @@ public class MobileMessageUtil {
     }
 
     /**
-     * 处理排单提醒-平台代发
+     * 处理排单提醒
      * @param phone
      * @param orderCode     订单编号
      * @return
      */
-    public static boolean dealQueueOrderPlatform(String phone, String orderCode){
-        String[] content = new String[1];
+    public boolean dealQueueOrderRemind(String phone, String orderCode, Integer sendType){
+        content = new String[3];
         content[0] = orderCode;
-        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.DEAL_QUEUE_ORDER_PLATFORM, content);
-        if (!"000000".equals(smsRes[0])) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * 处理排单提醒-自己发货
-     * @param phone
-     * @param orderCode     订单编号
-     * @param shipName      快递公司名称
-     * @param shipCode      快递单号
-     * @return
-     */
-    public static boolean dealQueueOrderSelf(String phone, String orderCode, String shipName, String shipCode){
-        String[] content = new String[3];
-        content[0] = orderCode;
-        content[1] = shipName;
-        content[2] = shipCode;
-        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.DEAL_QUEUE_ORDER_SELF, content);
+        content[1] = sendType==1?"查看在线库存":sendType==2?"耐心等待发货":"";
+        content[2] = EVENING_MESSAGE;
+        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.DEAL_QUEUE_ORDER_REMIND, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
         }
@@ -326,9 +280,10 @@ public class MobileMessageUtil {
      * @param skuName
      * @return
      */
-    public static boolean stockNotEnoughWarning(String phone, String skuName){
-        String[] content = new String[1];
+    public boolean stockNotEnoughWarning(String phone, String skuName){
+        content = new String[2];
         content[0] = skuName;
+        content[1] = EVENING_MESSAGE;
         String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.STOCK_NOT_ENOUGH_WARNINT, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
@@ -341,8 +296,10 @@ public class MobileMessageUtil {
      * @param phone
      * @return
      */
-    public static boolean newMallOrderRemind(String phone){
-        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.NEW_MALL_ORDER_REMIND, null);
+    public boolean newMallOrderRemind(String phone){
+        content = new String[1];
+        content[0] = EVENING_MESSAGE;
+        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.NEW_MALL_ORDER_REMIND, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
         }
@@ -355,9 +312,10 @@ public class MobileMessageUtil {
      * @param skuName   商品名称
      * @return
      */
-    public static boolean consumerOrderRemind(String phone, String skuName){
-        String[] content = new String[1];
+    public boolean consumerOrderRemind(String phone, String skuName){
+        content = new String[2];
         content[0] = skuName;
+        content[1] = EVENING_MESSAGE;
         String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.CONSUMER_ORDER_REMIND, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
@@ -371,9 +329,10 @@ public class MobileMessageUtil {
      * @param orderCode    订单编码
      * @return
      */
-    public static boolean consumerShipRemind(String phone, String orderCode){
-        String[] content = new String[1];
+    public boolean consumerShipRemind(String phone, String orderCode){
+        content = new String[2];
         content[0] = orderCode;
+        content[1] = EVENING_MESSAGE;
         String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.CONSUMER_SHIP_REMIND, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
@@ -387,13 +346,93 @@ public class MobileMessageUtil {
      * @param orderCode
      * @return
      */
-    public static boolean consumerConsumeSuccessRemind(String phone, String orderCode){
-        String[] content = new String[1];
+    public boolean consumerConsumeSuccessRemind(String phone, String orderCode){
+        content = new String[2];
         content[0] = orderCode;
+        content[1] = EVENING_MESSAGE;
         String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.CONSUMER_CONSUME_SUCCESS_REMIND, content);
         if (!"000000".equals(smsRes[0])) {
             return false;
         }
         return true;
+    }
+
+    /**
+     * 现申请审核-C端
+     * @param phone
+     * @param days     工作日天数
+     * @return
+     */
+    public boolean withdrawRequestVerifyCustomer(String phone, String days){
+        content = new String[2];
+        content[0] = days;
+        content[1] = EVENING_MESSAGE;
+        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.WITHDRAW_REQUEST_VERIFY_CUSTOMER, content);
+        if (!"000000".equals(smsRes[0])) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 消费者提现审核通过
+     * @param phone
+     * @param days      工作日天数
+     * @param theWay    打款方式{1:微信 2:支付宝 3:银行卡}
+     * @return
+     */
+    public boolean withdrawVerifyApproveCustomer(String phone, String days, Integer theWay){
+        content = new String[3];
+        content[0] = days;
+        content[1] = theWay==1?"微信":theWay==2?"支付宝":theWay==3?"银行卡":"";
+        content[2] = EVENING_MESSAGE;
+        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.WITHDRAW_VERIFY_APPROVE_CUSTOMER, content);
+        if (!"000000".equals(smsRes[0])) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 提现申请审核拒绝-c
+     * @param phone
+     * @param reason    拒绝原因
+     * @return
+     */
+    public boolean withdrawVerifyRefuseCustomer(String phone, String reason){
+        content = new String[2];
+        content[0] = reason;
+        content[1] = EVENING_MESSAGE;
+        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.WITHDRAW_VERIFY_REFUSE_CUSTOMER, content);
+        if (!"000000".equals(smsRes[0])) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 线下支付提醒
+     * @param phone
+     * @param orderCode
+     * @param amount
+     * @param mes
+     * @return
+     */
+    public static boolean offlinePaymentsRemind(String phone, String orderCode, String amount, String mes){
+        String[] content = new String[3];
+        content[0] = orderCode;
+        content[1] = amount;
+        content[2] = mes;
+        String[] smsRes = CCPRestSmsSDK.sendSMSWithResultMasiisShop(phone, SMSConstants.OFFLINE_PAYMENTS_REMIND, content);
+        if (!"000000".equals(smsRes[0])) {
+            return false;
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args){
+        MobileMessageUtil.getInitialization("B").consumerConsumeSuccessRemind("18800175968","111111");
+        MobileMessageUtil.getInitialization("B").certificationVerifyResult("18800175968",true);
     }
 }

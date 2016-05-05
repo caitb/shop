c9709022412fffb294312d27d406ee360b05227a
wangbingjian <bingjian.wang@masiis.com>
2016-05-05 18:31:22 +0800

    edit

--- shop-mall/src/main/java/com/masiis/shop/web/mall/controller/user/SfUserExtractApplyController.java
+++ shop-mall/src/main/java/com/masiis/shop/web/mall/controller/user/SfUserExtractApplyController.java
@@ -67,7 +67,11 @@ public class SfUserExtractApplyController extends BaseController{
         SfUserAccount userAccount = userAccountService.findAccountByUserId(userId);
         if (userAccount == null){
             userAccount = new SfUserAccount();
-            userAccount.setExtractableFee(new BigDecimal(0));
+            userAccount.setExtractableFee(new BigDecimal(0.00));
+            userAccount.setAppliedFee(new BigDecimal(0.00));
+        }
+        if (userAccount.getAppliedFee() == null){
+            userAccount.setAppliedFee(new BigDecimal(0.00));
         }
         mv.addObject("userAccount",userAccount);
         mv.addObject("comUser",comUser);

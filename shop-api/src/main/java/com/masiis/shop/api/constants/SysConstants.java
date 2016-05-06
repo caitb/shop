4063aefc8b7012f4035044068909ed7dc70fd83e
wangbingjian <bingjian.wang@masiis.com>
2016-05-06 09:53:32 +0800

    api-个人中心

--- shop-api/src/main/java/com/masiis/shop/api/constants/SysConstants.java
+++ shop-api/src/main/java/com/masiis/shop/api/constants/SysConstants.java
@@ -1,5 +1,7 @@
 package com.masiis.shop.api.constants;
 
+import com.masiis.shop.common.util.PropertiesUtils;
+
 /**
  * @Date 2016/5/3
  * @Auther lzh
@@ -10,4 +12,9 @@ public class SysConstants {
      * 商品最小图片
      */
     public static final String INDEX_PRODUCT_IMAGE_MIN = "index_product_220_220_url";
+
+    /**
+     * 商品等级图片
+     */
+    public static final String  AGENT_LEVEL_PRODUCT_ICON_URL = PropertiesUtils.getStringValue("agent_level_product_icon_url");
 }

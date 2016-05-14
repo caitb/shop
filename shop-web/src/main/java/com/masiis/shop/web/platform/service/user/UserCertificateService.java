acbdffe63dfc06b54c2f8fbb152031d0b7b2ec70
Tracy_jjh@163.com <jia13546550112>
2016-05-14 16:58:03 +0800

    授权书bug

--- shop-web/src/main/java/com/masiis/shop/web/platform/service/user/UserCertificateService.java
+++ shop-web/src/main/java/com/masiis/shop/web/platform/service/user/UserCertificateService.java
@@ -89,8 +89,9 @@ public class UserCertificateService {
         if(certificateInfo!=null && certificateInfo.getPid()!=0){
             ComUser comUser = comUserMapper.selectByPrimaryKey(certificateMapper.get(certificateInfo.getPid()).getUserId());
             pfc.setSjName(comUser.getRealName());
+        }else{
+            pfc.setSjName("平台");
         }
-        pfc.setSjName("平台");
         String ctValue = PropertiesUtils.getStringValue("index_user_certificate_url");
         pfc.setImgUrl(ctValue + pfc.getImgUrl());
         String beginTime = DateUtil.Date2String(pfc.getBeginTime(), "yyyy-MM-dd hh:mm:ss", null);

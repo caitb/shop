4389ecc885db100101ca364c18ab407e373c2886
wangbingjian <bingjian.wang@masiis.com>
2016-05-16 18:04:01 +0800

    edit

--- shop-scheduler/src/main/java/com/masiis/shop/scheduler/platform/service/agent/PfStatisticsAgentService.java
+++ shop-scheduler/src/main/java/com/masiis/shop/scheduler/platform/service/agent/PfStatisticsAgentService.java
@@ -1,7 +1,9 @@
 package com.masiis.shop.scheduler.platform.service.agent;
 
 import com.masiis.shop.common.exceptions.BusinessException;
+import com.masiis.shop.dao.platform.product.ComSkuMapper;
 import com.masiis.shop.dao.platform.user.PfUserSkuMapper;
+import com.masiis.shop.dao.po.ComSku;
 import com.masiis.shop.dao.po.PfUserSku;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,8 @@ public class PfStatisticsAgentService {
 
     @Autowired
     private PfUserSkuMapper pfUserSkuMapper;
+    @Autowired
+    private ComSkuMapper comSkuMapper;
     /**
      * 存放查询出来的所有倒数第二级
      */
@@ -31,7 +35,7 @@ public class PfStatisticsAgentService {
      */
     private Map<Integer,List> map;
     /**
-     * 上一级list
+     * 父list
      */
     private List<PfUserSku> parentList;
     /**
@@ -39,141 +43,143 @@ public class PfStatisticsAgentService {
      */
     private Integer currentLevel;
 
-    /**
-     * 每日统计代理下级人数
-     * @author:wbj
-     */
     public void statisticsAgent() throws Exception{
-        logger.info("每日统计代理下级人数");
-        logger.info("处理之前将下级代理数量都设置为0");
-        int updateCount = pfUserSkuMapper.updateResetAgentNum();
-        logger.info("受影响数量："+updateCount);
-        logger.info("查找倒数第二级");
-        secondLastList = pfUserSkuMapper.selectSecondLastLevel();
-        if (secondLastList == null || secondLastList.size() == 0){
-            logger.info("各个代理没有下一级代理");
-            return;
-        }
-        //遍历处理倒数第二级代理数据
-        Long count = 0l;
-        for (int i = 0; i < secondLastList.size(); i++){
-            PfUserSku pfUserSku = secondLastList.get(i);
-            count = pfUserSkuMapper.findLowerCount(pfUserSku.getId()).longValue();
-            if (count != pfUserSku.getAgentNum()){
-                pfUserSku.setAgentNum(count);
-                pfUserSkuUpdateList.add(pfUserSku);
-                secondLastList.set(i,pfUserSku);
-            }
-            currentLevel = pfUserSku.getAgentLevelId();
-        }
-        //处理数据以pid分组 Map<Integer,List>
-        lookForParent(secondLastList);
-        //计算上一级代理数量
-        parentList = computeParentAgentNum();
-        //递归处理
-        recursion(parentList);
-        //更新数据
-        try{
-            for (PfUserSku p : pfUserSkuUpdateList){
-                pfUserSkuMapper.updateByPrimaryKey(p);
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-            throw new BusinessException("更新代理数据失败");
-        }
-
     }
 
-    /**
-     * 将list以pid分组 放到map中
-     * @param pfUserSkus
-     */
-    private void lookForParent(List<PfUserSku> pfUserSkus){
-        logger.info("处理寻找上级代理");
-        List<PfUserSku> allList = pfUserSkuMapper.selectByLevel(currentLevel);
-        if (allList.size() == pfUserSkus.size()){
-            operateMap(pfUserSkus);
-        }else {
-            for (int i = 0; i < allList.size(); i++){
-                for (int j = 0; j < pfUserSkus.size(); j++){
-                    if (allList.get(i).getId() == pfUserSkus.get(j).getId()){
-                        allList.set(i,pfUserSkus.get(j));
-                    }
-                }
-            }
-            operateMap(allList);
-        }
-    }
+//    /**
+//     * 每日统计代理下级人数
+//     * @author:wbj
+//     */
+//    public void statisticsAgent() throws Exception{
+//        logger.info("每日统计代理下级人数");
+//        logger.info("处理之前将下级代理数量都设置为0");
+//        int updateCount = pfUserSkuMapper.updateResetAgentNum();
+//        logger.info("受影响数量："+updateCount);
+//        logger.info("查找倒数第二级");
+//        secondLastList = pfUserSkuMapper.selectSecondLastLevel();
+//        if (secondLastList == null || secondLastList.size() == 0){
+//            logger.info("各个代理没有下一级代理");
+//            return;
+//        }
+//        //遍历处理倒数第二级代理数据
+//        Long count = 0l;
+//        for (int i = 0; i < secondLastList.size(); i++){
+//            PfUserSku pfUserSku = secondLastList.get(i);
+//            count = pfUserSkuMapper.findLowerCount(pfUserSku.getId()).longValue();
+//            if (count != pfUserSku.getAgentNum()){
+//                pfUserSku.setAgentNum(count);
+//                pfUserSkuUpdateList.add(pfUserSku);
+//                secondLastList.set(i,pfUserSku);
+//            }
+//            currentLevel = pfUserSku.getAgentLevelId();
+//        }
+//        //处理数据以pid分组 Map<Integer,List>
+//        lookForParent(secondLastList);
+//        //计算上一级代理数量
+//        parentList = computeParentAgentNum();
+//        //递归处理
+//        recursion(parentList);
+//        //更新数据
+//        try{
+//            for (PfUserSku p : pfUserSkuUpdateList){
+//                pfUserSkuMapper.updateByPrimaryKey(p);
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//            throw new BusinessException("更新代理数据失败");
+//        }
+//    }
 
-    private void operateMap(List<PfUserSku> pfUserSkus){
-        map = new HashMap<>();
-        List<PfUserSku> list;
-        for (PfUserSku pfUserSku : pfUserSkus){
-            if (map.containsKey(pfUserSku.getPid())){
-                list = map.get(pfUserSku.getPid());
-                list.add(pfUserSku);
-                map.put(pfUserSku.getPid(),list);
-            } else {
-                list = new ArrayList<>();
-                list.add(pfUserSku);
-                map.put(pfUserSku.getPid(),list);
-            }
-        }
-    }
-
-    /**
-     * 计算上一级代理数量
-     * @return
-     */
-    private List<PfUserSku> computeParentAgentNum(){
-        logger.info("计算上一级代理数量");
-        Iterator it = map.entrySet().iterator();
-        List<Integer> pids = new ArrayList<>();
-        while (it.hasNext()){
-            Map.Entry entry = (Map.Entry) it.next();
-            Integer key = (Integer) entry.getKey();
-            pids.add(key);
-        }
-
-        List<PfUserSku> pfUserSkus = null;
-        if (pids.size() == 0){
-            return pfUserSkus;
-        }
-        pfUserSkus = pfUserSkuMapper.selectByListId(pids);
-        List<PfUserSku> value;
-        for (int i = 0; i < pfUserSkus.size(); i++){
-            Long sumAgentnum = 0l;
-            PfUserSku pfUserSku = pfUserSkus.get(i);
-            logger.info(pfUserSku.getId());
-            value = map.get(pfUserSku.getId());
-            for (PfUserSku userSku : value){
-                sumAgentnum += userSku.getAgentNum();
-            }
-            sumAgentnum += value.size();
-            if (pfUserSku.getAgentNum() != sumAgentnum){
-                pfUserSku.setAgentNum(sumAgentnum);
-                pfUserSkus.set(i,pfUserSku);
-                pfUserSkuUpdateList.add(pfUserSku);
-            }
-        }
-        return pfUserSkus;
-    }
-
-    /**
-     * 递归处理
-     * 将list以pid分组 放到map中
-     * 计算上一级代理数量
-     * @param list
-     */
-    private void recursion(List<PfUserSku> list){
-        logger.info("递归处理");
-        //处理数据以pid分组 Map<Integer,List>
-        lookForParent(list);
-        //计算上一级代理数量
-        parentList = computeParentAgentNum();
-        if (parentList != null && parentList.size() > 0){
-            currentLevel = currentLevel - 1;
-            recursion(parentList);
-        }
-    }
+//    /**
+//     * 将list以pid分组 放到map中
+//     * @param pfUserSkus
+//     */
+//    private void lookForParent(List<PfUserSku> pfUserSkus){
+//        logger.info("处理寻找上级代理");
+//        List<PfUserSku> allList = pfUserSkuMapper.selectByLevel(currentLevel);
+//        if (allList.size() == pfUserSkus.size()){
+//            operateMap(pfUserSkus);
+//        }else {
+//            for (int i = 0; i < allList.size(); i++){
+//                for (int j = 0; j < pfUserSkus.size(); j++){
+//                    if (allList.get(i).getId() == pfUserSkus.get(j).getId()){
+//                        allList.set(i,pfUserSkus.get(j));
+//                    }
+//                }
+//            }
+//            operateMap(allList);
+//        }
+//    }
+//
+//    private void operateMap(List<PfUserSku> pfUserSkus){
+//        map = new HashMap<>();
+//        List<PfUserSku> list;
+//        for (PfUserSku pfUserSku : pfUserSkus){
+//            if (map.containsKey(pfUserSku.getPid())){
+//                list = map.get(pfUserSku.getPid());
+//                list.add(pfUserSku);
+//                map.put(pfUserSku.getPid(),list);
+//            } else {
+//                list = new ArrayList<>();
+//                list.add(pfUserSku);
+//                map.put(pfUserSku.getPid(),list);
+//            }
+//        }
+//    }
+//
+//    /**
+//     * 计算上一级代理数量
+//     * @return
+//     */
+//    private List<PfUserSku> computeParentAgentNum(){
+//        logger.info("计算上一级代理数量");
+//        Iterator it = map.entrySet().iterator();
+//        List<Integer> pids = new ArrayList<>();
+//        while (it.hasNext()){
+//            Map.Entry entry = (Map.Entry) it.next();
+//            Integer key = (Integer) entry.getKey();
+//            pids.add(key);
+//        }
+//
+//        List<PfUserSku> pfUserSkus = null;
+//        if (pids.size() == 0){
+//            return pfUserSkus;
+//        }
+//        pfUserSkus = pfUserSkuMapper.selectByListId(pids);
+//        List<PfUserSku> value;
+//        for (int i = 0; i < pfUserSkus.size(); i++){
+//            Long sumAgentnum = 0l;
+//            PfUserSku pfUserSku = pfUserSkus.get(i);
+//            logger.info(pfUserSku.getId());
+//            value = map.get(pfUserSku.getId());
+//            for (PfUserSku userSku : value){
+//                sumAgentnum += userSku.getAgentNum();
+//            }
+//            sumAgentnum += value.size();
+//            if (pfUserSku.getAgentNum() != sumAgentnum){
+//                pfUserSku.setAgentNum(sumAgentnum);
+//                pfUserSkus.set(i,pfUserSku);
+//                pfUserSkuUpdateList.add(pfUserSku);
+//            }
+//        }
+//        return pfUserSkus;
+//    }
+//
+//    /**
+//     * 递归处理
+//     * 将list以pid分组 放到map中
+//     * 计算上一级代理数量
+//     * @param list
+//     */
+//    private void recursion(List<PfUserSku> list){
+//        logger.info("递归处理");
+//        //处理数据以pid分组 Map<Integer,List>
+//        lookForParent(list);
+//        //计算上一级代理数量
+//        parentList = computeParentAgentNum();
+//        if (parentList != null && parentList.size() > 0){
+//            currentLevel = currentLevel - 1;
+//            recursion(parentList);
+//        }
+//    }
 }

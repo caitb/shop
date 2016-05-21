e100234e011b45865566fa8e568c15a68add82fc
hanzengzhi <516490578@qq.com>
2016-05-21 16:40:19 +0800

    查询所有省市区json文件

---
+++ shop-api/src/main/java/com/masiis/shop/api/bean/user/Province.java
@@ -0,0 +1,45 @@
+package com.masiis.shop.api.bean.user;
+
+import java.util.List;
+
+/**
+ * Created by hzz on 2016/5/21.
+ */
+public class Province {
+    private Integer id;
+    private Integer pid;
+    private String name;
+    private List<City> cities;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<City> getCities() {
+        return cities;
+    }
+
+    public void setCities(List<City> cities) {
+        this.cities = cities;
+    }
+}

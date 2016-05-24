a6852719e52dc85bab856f07924243effab5cffb
luozhihui <767508395@qq.com>
2016-05-24 10:50:09 +0800

    maven修改

--- shop-api/src/main/java/com/masiis/shop/api/controller/user/PhoneController.java
+++ shop-api/src/main/java/com/masiis/shop/api/controller/user/PhoneController.java
@@ -2,6 +2,8 @@ package com.masiis.shop.api.controller.user;
 
 import com.masiis.shop.api.bean.base.BaseReq;
 import com.masiis.shop.api.bean.base.BaseRes;
+import com.masiis.shop.api.bean.system.GetPhoneValidCodeReq;
+import com.masiis.shop.api.bean.system.GetPhoneValidCodeRes;
 import com.masiis.shop.api.bean.user.BindPhoneReq;
 import com.masiis.shop.api.bean.user.BindPhoneRes;
 import com.masiis.shop.api.bean.user.CheckPUserPhoneReq;
@@ -12,6 +14,11 @@ import com.masiis.shop.api.controller.base.BaseController;
 import com.masiis.shop.api.service.product.SkuService;
 import com.masiis.shop.api.service.user.ComUserService;
 import com.masiis.shop.api.service.user.UserSkuService;
+import com.masiis.shop.api.utils.SpringRedisUtil;
+import com.masiis.shop.api.utils.ValidCodeUtils;
+import com.masiis.shop.common.constant.SMSConstants;
+import com.masiis.shop.common.exceptions.BusinessException;
+import com.masiis.shop.common.util.MobileMessageUtil;
 import com.masiis.shop.common.util.PhoneNumUtils;
 import com.masiis.shop.dao.po.ComSku;
 import com.masiis.shop.dao.po.ComUser;
@@ -24,6 +31,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 /**
  * @Date 2016/5/24
@@ -107,6 +115,68 @@ public class PhoneController extends BaseController {
     public BindPhoneRes bindPhone(HttpServletRequest request, BindPhoneReq req, ComUser user){
 
         return null;
+    }
+
+    @RequestMapping("/vcode")
+    @ResponseBody
+    @SignValid(paramType = GetPhoneValidCodeReq.class)
+    public GetPhoneValidCodeRes getPhoneValidCode(HttpServletRequest request, GetPhoneValidCodeReq req){
+        GetPhoneValidCodeRes res = new GetPhoneValidCodeRes();
+        try {
+            if(req == null){
+                throw new BusinessException();
+            }
+            if (StringUtils.isBlank(req.getPhoneNum())) {
+                // 电话号码为空
+                res.setResCode(SysResCodeCons.RES_CODE_PHONENUM_BLANK);
+                res.setResMsg(SysResCodeCons.RES_CODE_PHONENUM_BLANK_MSG);
+                throw new BusinessException(SysResCodeCons.RES_CODE_PHONENUM_BLANK_MSG);
+            }
+            String phoneNum = req.getPhoneNum().trim();
+            // 检测手机号格式
+            if(!PhoneNumUtils.isPhoneNum(phoneNum)){
+                // 手机号码格式不正确
+                res.setResCode(SysResCodeCons.RES_CODE_PHONENUM_INVALID);
+                res.setResMsg(SysResCodeCons.RES_CODE_PHONENUM_INVALID_MSG);
+                throw new BusinessException(SysResCodeCons.RES_CODE_PHONENUM_INVALID_MSG);
+            }
+            // 查询请求频率
+            Date exTime = SpringRedisUtil.get(ValidCodeUtils.getRdPhoneNumVcodeNextOpTimeName(phoneNum), Date.class);
+            if(exTime != null && exTime.compareTo(new Date()) > 0){
+                res.setResCode(SysResCodeCons.RES_CODE_VALIDCODE_REQ_OFTEN);
+                res.setResMsg(SysResCodeCons.RES_CODE_VALIDCODE_REQ_OFTEN_MSG);
+                throw new BusinessException(SysResCodeCons.RES_CODE_VALIDCODE_REQ_OFTEN_MSG);
+            }
+            // 获取验证码
+            String code = ValidCodeUtils.generareValidCode();
+            Date exNewTime = new Date();
+            exNewTime.setTime(exNewTime.getTime() + 60 * 1000);
+            // 发送短信
+            if(!MobileMessageUtil.getInitialization("B").verificationCode(phoneNum, code, SMSConstants.REGESTER_VALID_TIME)){
+                // 验证码短信发送失败
+                res.setResCode(SysResCodeCons.RES_CODE_VALIDCODE_SMS_FAIL);
+                res.setResMsg(SysResCodeCons.RES_CODE_VALIDCODE_SMS_FAIL_MSG);
+                throw new BusinessException(SysResCodeCons.RES_CODE_VALIDCODE_SMS_FAIL_MSG);
+            }
+            // 保存验证码到redis
+            SpringRedisUtil.saveEx(ValidCodeUtils.getRdPhoneNumVcodeNextOpTimeName(phoneNum),
+                    exNewTime, 60);
+            SpringRedisUtil.saveEx(ValidCodeUtils.getRedisPhoneNumValidCodeName(phoneNum), code,
+                    Integer.valueOf(SMSConstants.REGESTER_VALID_TIME) * 60);
+            // 返回结果
+            res.setValidcode(code);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            if(StringUtils.isBlank(res.getResCode())){
+                res.setResCode(SysResCodeCons.RES_CODE_NOT_KNOWN);
+                res.setResMsg(SysResCodeCons.RES_CODE_NOT_KNOWN_MSG);
+            }
+            return res;
+        }
+        // 返回结果
+        res.setResCode(SysResCodeCons.RES_CODE_SUCCESS);
+        res.setResMsg(SysResCodeCons.RES_CODE_SUCCESS_MSG);
+        return res;
     }
 
 }

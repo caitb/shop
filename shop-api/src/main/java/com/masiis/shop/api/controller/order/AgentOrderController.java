143579ba582868ae466a3b770698b7f283cc7d3a
luozhihui <767508395@qq.com>
2016-05-25 10:11:07 +0800

    mshop修改aliyun-oss私服jar

---
+++ shop-api/src/main/java/com/masiis/shop/api/controller/order/AgentOrderController.java
@@ -0,0 +1,14 @@
+package com.masiis.shop.api.controller.order;
+
+import com.masiis.shop.api.controller.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Date 2016/5/24
+ * @Auther lzh
+ */
+@Controller
+@RequestMapping("/agent")
+public class AgentOrderController extends BaseController {
+}

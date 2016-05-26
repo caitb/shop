31fe134b7afef1c5a4bb1530ba727907aa8ab12c
luozhihui <767508395@qq.com>
2016-05-26 14:49:06 +0800

    shop-api创建代理订单暂时提交

--- shop-api/src/main/java/com/masiis/shop/api/bean/order/BAgentOrderAddReq.java
+++ shop-api/src/main/java/com/masiis/shop/api/bean/order/BAgentOrderAddReq.java
@@ -7,5 +7,49 @@ import com.masiis.shop.api.bean.base.BaseBusinessReq;
  * @Auther lzh
  */
 public class BAgentOrderAddReq extends BaseBusinessReq {
+    private Integer skuId;
+    private Integer agentLevelId;
+    private String wxId;
+    private Long userAddressId;
+    private String userMessage;
 
+    public Integer getSkuId() {
+        return skuId;
+    }
+
+    public void setSkuId(Integer skuId) {
+        this.skuId = skuId;
+    }
+
+    public Integer getAgentLevelId() {
+        return agentLevelId;
+    }
+
+    public void setAgentLevelId(Integer agentLevelId) {
+        this.agentLevelId = agentLevelId;
+    }
+
+    public String getWxId() {
+        return wxId;
+    }
+
+    public void setWxId(String wxId) {
+        this.wxId = wxId;
+    }
+
+    public Long getUserAddressId() {
+        return userAddressId;
+    }
+
+    public void setUserAddressId(Long userAddressId) {
+        this.userAddressId = userAddressId;
+    }
+
+    public String getUserMessage() {
+        return userMessage;
+    }
+
+    public void setUserMessage(String userMessage) {
+        this.userMessage = userMessage;
+    }
 }

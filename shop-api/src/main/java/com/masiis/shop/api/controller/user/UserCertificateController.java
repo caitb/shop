0dfa477ae7553b265f57fb6be3bb059175748e79
Tracy_jjh@163.com <jia13546550112>
2016-05-26 14:37:26 +0800

    证书API update

--- shop-api/src/main/java/com/masiis/shop/api/controller/user/UserCertificateController.java
+++ shop-api/src/main/java/com/masiis/shop/api/controller/user/UserCertificateController.java
@@ -6,11 +6,15 @@ import com.masiis.shop.api.constants.SignValid;
 import com.masiis.shop.api.constants.SysResCodeCons;
 import com.masiis.shop.api.controller.base.BaseController;
 import com.masiis.shop.api.service.product.SkuService;
+import com.masiis.shop.api.service.user.ComUserService;
 import com.masiis.shop.api.service.user.UserCertificateService;
+import com.masiis.shop.api.service.user.UserSkuService;
+import com.masiis.shop.common.util.PropertiesUtils;
 import com.masiis.shop.dao.beans.certificate.CertificateInfo;
 import com.masiis.shop.dao.po.ComSku;
 import com.masiis.shop.dao.po.ComUser;
 import com.masiis.shop.dao.po.PfUserCertificate;
+import com.masiis.shop.dao.po.PfUserSku;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 /**
@@ -33,6 +38,12 @@ public class UserCertificateController extends BaseController {
     private UserCertificateService userCertificateService;
     @Resource
     private SkuService skuService;
+    @Resource
+    private UserSkuService userSkuService;
+    @Resource
+    private ComUserService comUserService;
+
+
 
     /**
       * @Author jjh
@@ -92,12 +103,28 @@ public class UserCertificateController extends BaseController {
     @RequestMapping("/upperInfo")
     @ResponseBody
     @SignValid(paramType = CommonReq.class)
-    public UserCertificateRes upperInfoOfCertificate(HttpServletRequest request, CommonReq req, ComUser user){
+    public UserCertificateRes upperInfoOfCertificate(HttpServletRequest request, CommonReq req, ComUser user) {
         UserCertificateRes userCertificateRes = new UserCertificateRes();
         try {
-
+            PfUserSku pfUserSku = userSkuService.getUserSkuById(req.getId().intValue());
+            ComUser userInfo = comUserService.getUserById(pfUserSku.getUserId());
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            String sDate = sdf.format(userInfo.getCreateTime());
+            ComSku comSku = skuService.getSkuById(pfUserSku.getSkuId());
+            PfUserCertificate pfUserCertificate = userCertificateService.getCertificateBypfuId(pfUserSku.getId());
+            String ctName = userCertificateService.getCtname(pfUserCertificate.getAgentLevelId());
+            String ctValue = PropertiesUtils.getStringValue("index_user_certificate_url");
+            pfUserCertificate.setImgUrl(ctValue + pfUserCertificate.getImgUrl());
+            userCertificateRes.setRealName(userInfo.getRealName());
+            userCertificateRes.setSkuName(comSku.getName());
+            userCertificateRes.setCtname(ctName);
+            userCertificateRes.setPfUserCertificate(pfUserCertificate);
+            userCertificateRes.setsDate(sDate);
+            userCertificateRes.setResCode(SysResCodeCons.RES_CODE_SUCCESS);
+            userCertificateRes.setResMsg(SysResCodeCons.RES_CODE_SUCCESS_MSG);
         } catch (Exception e) {
-
+            userCertificateRes.setResCode(SysResCodeCons.RES_CODE_NOT_KNOWN);
+            userCertificateRes.setResMsg(e.getMessage());
         }
         return userCertificateRes;
     }

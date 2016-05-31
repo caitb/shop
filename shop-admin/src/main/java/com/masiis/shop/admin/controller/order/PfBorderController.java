ea4bde39e5d86146c0ed0085c0da95e04361ba91
zhaoliang <370787497@qq.com>
2016-05-31 19:37:11 +0800

    去掉快捷支付

--- shop-admin/src/main/java/com/masiis/shop/admin/controller/order/PfBorderController.java
+++ shop-admin/src/main/java/com/masiis/shop/admin/controller/order/PfBorderController.java
@@ -43,7 +43,7 @@ public class PfBorderController extends BaseController {
     private BOrderPayService bOrderPayService;
 
     @RequestMapping("/list.shtml")
-    public String list(){
+    public String list() {
         return "order/border/list";
     }
 
@@ -54,14 +54,14 @@ public class PfBorderController extends BaseController {
                        Integer pageSize,
                        String sortName,
                        String sortOrder,
-                       PfBorder pfBorder){
+                       PfBorder pfBorder) {
 
         try {
             Map<String, Object> pageMap = bOrderService.listByCondition(pageNumber, pageSize, sortName, sortOrder, pfBorder);
 
             return pageMap;
         } catch (Exception e) {
-            log.error("查询合伙人列表失败![pfBorder="+pfBorder+"]");
+            log.error("查询合伙人列表失败![pfBorder=" + pfBorder + "]");
             e.printStackTrace();
         }
 
@@ -69,7 +69,7 @@ public class PfBorderController extends BaseController {
     }
 
     @RequestMapping("detail.shtml")
-    public ModelAndView detail(HttpServletRequest request, HttpServletResponse response, Long borderId){
+    public ModelAndView detail(HttpServletRequest request, HttpServletResponse response, Long borderId) {
         try {
             ModelAndView mav = new ModelAndView("order/border/detail");
 
@@ -79,7 +79,7 @@ public class PfBorderController extends BaseController {
 
             return mav;
         } catch (Exception e) {
-            log.error("查看合伙人订单明细失败![borderId="+borderId+"]");
+            log.error("查看合伙人订单明细失败![borderId=" + borderId + "]");
             e.printStackTrace();
         }
 
@@ -87,12 +87,13 @@ public class PfBorderController extends BaseController {
     }
 
     @RequestMapping("/offline/list.shtml")
-    public String offlineList(){
+    public String offlineList() {
         return "order/border/offlinePayList";
     }
 
     /**
      * 合伙人线下支付订单
+     *
      * @param request
      * @param response
      * @param pageNumber
@@ -104,11 +105,11 @@ public class PfBorderController extends BaseController {
     @RequestMapping("/offline/list.do")
     @ResponseBody
     public Object offlineList(HttpServletRequest request, HttpServletResponse response,
-                       Integer pageNumber,
-                       Integer pageSize,
-                       String sortName,
-                       String sortOrder,
-                       PfBorder pfBorder){
+                              Integer pageNumber,
+                              Integer pageSize,
+                              String sortName,
+                              String sortOrder,
+                              PfBorder pfBorder) {
 
         try {
             pfBorder.setOrderStatus(9);
@@ -116,7 +117,7 @@ public class PfBorderController extends BaseController {
 
             return pageMap;
         } catch (Exception e) {
-            log.error("查询合伙人线下支付订单列表失败![pfBorder="+pfBorder+"]");
+            log.error("查询合伙人线下支付订单列表失败![pfBorder=" + pfBorder + "]");
             e.printStackTrace();
         }
 
@@ -125,23 +126,25 @@ public class PfBorderController extends BaseController {
 
     /**
      * 合伙人线下支付订单收款确认
+     *
      * @param request
      * @param response
-     * @param bOrderId  合伙人订单ID
-     * @param outOrderId  银行流水号
+     * @param bOrderId   合伙人订单ID
+     * @param outOrderId 银行流水号
      * @return
      */
     @RequestMapping("/offline/Receipt.do")
     @ResponseBody
-    public Object Receipt(HttpServletRequest request, HttpServletResponse response, Long bOrderId, String outOrderId){
+    public Object Receipt(HttpServletRequest request, HttpServletResponse response, Long bOrderId, String outOrderId) {
 
         try {
             PfBorderPayment borderPayment = bOrderPaymentService.findOfflinePayByBOrderId(bOrderId);
-            bOrderPayService.mainPayBOrder(borderPayment, outOrderId, request.getServletContext().getRealPath("/"),getPbUser(request));
+            //,getPbUser(request)
+            bOrderPayService.mainPayBOrder(borderPayment, outOrderId, request.getServletContext().getRealPath("/"));
 
             return "success";
         } catch (Exception e) {
-            log.error("合伙人线下支付收款确认失败![bOrderId="+bOrderId+"][outOrderId="+outOrderId+"]");
+            log.error("合伙人线下支付收款确认失败![bOrderId=" + bOrderId + "][outOrderId=" + outOrderId + "]");
             e.printStackTrace();
         }
 
@@ -150,20 +153,21 @@ public class PfBorderController extends BaseController {
 
     /**
      * 处理排单
+     *
      * @param request
      * @param response
-     * @param borderId  订单id
-     * @param sendType  发货类型
+     * @param borderId 订单id
+     * @param sendType 发货类型
      * @return
      */
     @RequestMapping("/scheduling.do")
     @ResponseBody
-    public Object scheduling(HttpServletRequest request, HttpServletResponse response, Long borderId, String sendType){
+    public Object scheduling(HttpServletRequest request, HttpServletResponse response, Long borderId, String sendType) {
 
         Map<Long, String> orderMap = new HashMap<>();
         try {
-            if(borderId == null || sendType == null){
-                throw new RuntimeException("参数有误!不知道处理哪个订单![orderMap="+orderMap+"]");
+            if (borderId == null || sendType == null) {
+                throw new RuntimeException("参数有误!不知道处理哪个订单![orderMap=" + orderMap + "]");
             }
             orderMap.put(borderId, sendType);
 
@@ -171,7 +175,7 @@ public class PfBorderController extends BaseController {
 
             return "success";
         } catch (Exception e) {
-            log.error("处理排单出错了![orderMap="+orderMap+"]");
+            log.error("处理排单出错了![orderMap=" + orderMap + "]");
             e.printStackTrace();
         }
 
@@ -180,6 +184,7 @@ public class PfBorderController extends BaseController {
 
     /**
      * 发货
+     *
      * @param request
      * @param response
      * @param pfBorderFreight
@@ -188,20 +193,20 @@ public class PfBorderController extends BaseController {
     @RequestMapping("/delivery.do")
     @ResponseBody
     public Object delivery(HttpServletRequest request, HttpServletResponse response,
-                           PfBorderFreight pfBorderFreight){
+                           PfBorderFreight pfBorderFreight) {
 
         try {
-            if (pfBorderFreight.getShipManId() == null){
+            if (pfBorderFreight.getShipManId() == null) {
                 return "请选择一个快递";
             }
-            if(StringUtils.isBlank(pfBorderFreight.getFreight())){
+            if (StringUtils.isBlank(pfBorderFreight.getFreight())) {
                 return "请填写运单号";
             }
-            bOrderService.delivery(pfBorderFreight,getPbUser(request));
+            bOrderService.delivery(pfBorderFreight, getPbUser(request));
 
             return "success";
         } catch (Exception e) {
-            log.error("合伙人订单发货失败![pfBorderFreight="+pfBorderFreight+"]");
+            log.error("合伙人订单发货失败![pfBorderFreight=" + pfBorderFreight + "]");
             e.printStackTrace();
         }
 

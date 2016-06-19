5bcc70f048aab937a553206d13b5b28a63b85252
wangbingjian <bingjian.wang@masiis.com>
2016-06-19 15:15:26 +0800

    edit 代理升级

---
+++ shop-web/src/main/java/com/masiis/shop/web/platform/service/user/UpgradeWechatNewsService.java
@@ -0,0 +1,13 @@
+package com.masiis.shop.web.platform.service.user;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 代理升级微信消息通知Service
+ * Created by wangbingjian on 2016/6/18.
+ */
+@Service
+@Transactional
+public class UpgradeWechatNewsService {
+}

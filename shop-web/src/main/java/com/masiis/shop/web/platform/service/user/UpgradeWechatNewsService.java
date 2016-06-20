35ab02443fd134833280f646244d5b811498ab2b
zhaoliang <370787497@qq.com>
2016-06-20 16:43:27 +0800

    升级，V1.2推荐关系

--- shop-web/src/main/java/com/masiis/shop/web/platform/service/user/UpgradeWechatNewsService.java
+++ shop-web/src/main/java/com/masiis/shop/web/platform/service/user/UpgradeWechatNewsService.java
@@ -66,7 +66,7 @@ public class UpgradeWechatNewsService {
         }else{
             //上级变化
             //给原上级发微信
-            String[] _param = new String[4];
+            String[] _param = new String[1];
             _param[0] = comUser.getRealName();
             WxPFNoticeUtils.getInstance().upgradeApplyResultNotice(oldUser,_param,url,false);
             //给新的上级发

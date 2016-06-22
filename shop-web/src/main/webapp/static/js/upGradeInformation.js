d90a3145a8d04c4889d2e4744e7c49fab08a5671
wangbingjian <bingjian.wang@masiis.com>
2016-06-22 21:16:20 +0800

    edit

--- shop-web/src/main/webapp/static/js/upGradeInformation.js
+++ shop-web/src/main/webapp/static/js/upGradeInformation.js
@@ -20,7 +20,7 @@ $(".que_que").on("click",function(){
                     blackHide();
                     $("#floor3").html("").append("<h1>您已选择暂不升级，您的下级将会与您解除关系。您可以获得一次性奖励</h1>");
                     $("#floor2").html("");
-                    $("status").text(data.status);
+                    $("#status").text(data.status);
                     //var newUp = data.newUp;
                     notUpgradeMessage(upgradeId);
                 }else {

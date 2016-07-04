53b6637a2679c2a2af1a4a859eccd72a7638dd45
hanzengzhi <516490578@qq.com>
2016-07-04 18:51:35 +0800

    增加活动详情

---
+++ shop-dao/src/main/java/com/masiis/shop/dao/beans/promotion/PromotionGiftDetail.java
@@ -0,0 +1,12 @@
+package com.masiis.shop.dao.beans.promotion;
+
+/**
+ *  活动页面--领取奖励详情
+ */
+public class PromotionGiftDetail {
+
+    private Integer promoId;    //活动id
+    private Integer promoRuleId;    //活动规则id
+    private Integer promoGiftId;    //活动奖品id
+
+}

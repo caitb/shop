f07ac784b758aae2e08dfa2f9cbe60862fc56c42
caitb <caitb@139.com>
2016-07-05 19:57:08 +0800

    素材模块

--- shop-admin/src/main/java/com/masiis/shop/admin/controller/material/MaterialController.java
+++ shop-admin/src/main/java/com/masiis/shop/admin/controller/material/MaterialController.java
@@ -9,15 +9,13 @@ import com.masiis.shop.dao.beans.material.MaterialLibrary;
 import com.masiis.shop.dao.po.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 素材库
@@ -108,6 +106,59 @@ public class MaterialController extends BaseController {
             resultMap.put("code", "fail");
             resultMap.put("msg", "保存失败!");
             log.error("保存素材组失败![comSkuMaterialGroup="+comSkuMaterialGroup+"]"+e);
+            e.printStackTrace();
+        }
+
+        return resultMap;
+    }
+
+    /**
+     * 保存素材
+     * @param comSkuMaterial
+     * @return
+     */
+    @RequestMapping("/saveMaterial.do")
+    @ResponseBody
+    public Map<String, Object> saveMaterial(
+                                            HttpServletRequest request,
+                                            ComSkuMaterial comSkuMaterial,
+                                            @RequestParam(value = "types",       required = false)Integer[] types,
+                                            @RequestParam(value = "fileNames",   required = false)String[]  fileNames,
+                                            @RequestParam(value = "fileUrls",    required = false)String[]  fileUrls,
+                                            @RequestParam(value = "fileSuffixs", required = false)String[]  fileSuffixs,
+                                            @RequestParam(value = "fileSizes",   required = false)Long[]  fileSizes) {
+        Map<String, Object> resultMap = new HashMap<>();
+
+        try {
+            PbUser pbUser = getPbUser(request);
+
+            comSkuMaterial.setCreateTime(new Date());
+            comSkuMaterial.setCreateMan(pbUser.getId());
+            comSkuMaterial.setSort(0);
+            List<ComSkuMaterialItem> comSkuMaterialItems = new ArrayList<>();
+            for(int i=0; i<types.length; i++){
+                ComSkuMaterialItem comSkuMaterialItem = new ComSkuMaterialItem();
+                comSkuMaterialItem.setCreateTime(new Date());
+                comSkuMaterialItem.setCreateMan(pbUser.getId());
+                comSkuMaterialItem.setType(types[0]);
+                comSkuMaterialItem.setFileName(fileNames[i]);
+                comSkuMaterialItem.setFileUrl(fileUrls[i]);
+                comSkuMaterialItem.setFileSuffix(fileSuffixs[i]);
+                comSkuMaterialItem.setFileSize(fileSizes[i]);
+                comSkuMaterialItem.setMaterialLibraryId(comSkuMaterial.getMaterialLibraryId());
+                comSkuMaterialItem.setMaterialGroupId(comSkuMaterial.getMaterialGroupId());
+
+                comSkuMaterialItems.add(comSkuMaterialItem);
+            }
+
+            materialService.saveMaterial(comSkuMaterial, comSkuMaterialItems);
+
+            resultMap.put("code", "success");
+            resultMap.put("msg", "保存成功!");
+        } catch (Exception e) {
+            resultMap.put("code", "fail");
+            resultMap.put("msg", "保存失败!");
+            log.error("保存素材失败![comSkuMaterial="+comSkuMaterial+"]"+e);
             e.printStackTrace();
         }
 
--- shop-admin/src/main/java/com/masiis/shop/admin/service/material/MaterialService.java
+++ shop-admin/src/main/java/com/masiis/shop/admin/service/material/MaterialService.java
@@ -3,10 +3,12 @@ package com.masiis.shop.admin.service.material;
 import com.masiis.shop.common.exceptions.BusinessException;
 import com.masiis.shop.dao.beans.material.MaterialLibrary;
 import com.masiis.shop.dao.platform.material.ComSkuMaterialGroupMapper;
+import com.masiis.shop.dao.platform.material.ComSkuMaterialItemMapper;
 import com.masiis.shop.dao.platform.material.ComSkuMaterialLibraryMapper;
 import com.masiis.shop.dao.platform.material.ComSkuMaterialMapper;
 import com.masiis.shop.dao.po.ComSkuMaterial;
 import com.masiis.shop.dao.po.ComSkuMaterialGroup;
+import com.masiis.shop.dao.po.ComSkuMaterialItem;
 import com.masiis.shop.dao.po.ComSkuMaterialLibrary;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +29,8 @@ public class MaterialService {
     private ComSkuMaterialGroupMapper comSkuMaterialGroupMapper;
     @Resource
     private ComSkuMaterialLibraryMapper comSkuMaterialLibraryMapper;
+    @Resource
+    private ComSkuMaterialItemMapper comSkuMaterialItemMapper;
 
     /**
      * 加载所有素材库
@@ -50,6 +54,19 @@ public class MaterialService {
      */
     public void saveMaterialGroup(ComSkuMaterialGroup comSkuMaterialGroup) {
         comSkuMaterialGroupMapper.insert(comSkuMaterialGroup);
+    }
+
+    /**
+     * 保存素材
+     * @param comSkuMaterial
+     * @param comSkuMaterialItems
+     */
+    public void saveMaterial(ComSkuMaterial comSkuMaterial, List<ComSkuMaterialItem> comSkuMaterialItems) {
+        comSkuMaterialMapper.insert(comSkuMaterial);
+        for(ComSkuMaterialItem comSkuMaterialItem : comSkuMaterialItems){
+            comSkuMaterialItem.setMaterialId(comSkuMaterial.getId());
+            comSkuMaterialItemMapper.insert(comSkuMaterialItem);
+        }
     }
 
     /**

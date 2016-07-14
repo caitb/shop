913adea291d3f7759531635bdf97d0ea96fa8e75
wangbingjian <bingjian.wang@masiis.com>
2016-07-14 11:04:26 +0800

    edit

--- shop-dao/src/main/java/com/masiis/shop/dao/platform/message/PfMessageSrRelationMapper.java
+++ shop-dao/src/main/java/com/masiis/shop/dao/platform/message/PfMessageSrRelationMapper.java
@@ -7,14 +7,11 @@
  */
 package com.masiis.shop.dao.platform.message;
 
-import com.masiis.shop.dao.beans.message.PfMessageCenterDetail;
 import com.masiis.shop.dao.po.PfMessageContent;
 import com.masiis.shop.dao.po.PfMessageSrRelation;
-import com.sun.org.glassfish.external.statistics.annotations.Reset;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.Resource;
 import java.util.List;
 @Repository
 public interface PfMessageSrRelationMapper {

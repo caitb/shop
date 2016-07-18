513fc04180dfcf67af89be2fd5357ed2543b8aea
luozhihui <767508395@qq.com>
2016-07-18 18:07:41 +0800

    中奖定时通知暂时提交

---
+++ shop-scheduler/src/main/java/com/masiis/shop/scheduler/task/mall/SfPromotionTask.java
@@ -0,0 +1,19 @@
+package com.masiis.shop.scheduler.task.mall;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Date 2016/7/18
+ * @Author lzh
+ */
+@Component
+public class SfPromotionTask {
+    private Logger log = Logger.getLogger(this.getClass());
+
+
+
+    public void prizeResultWxNoticeJob(){
+
+    }
+}

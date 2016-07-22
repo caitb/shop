157c85689dd47d1c00f44ed8b1f7362ae6b3c812
luozhihui <767508395@qq.com>
2016-07-22 18:08:24 +0800

    修改合并bug

--- shop-admin/src/main/java/com/masiis/shop/admin/controller/storage/StorageChangeController.java
+++ shop-admin/src/main/java/com/masiis/shop/admin/controller/storage/StorageChangeController.java
@@ -21,7 +21,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import sun.org.mozilla.javascript.internal.EcmaError;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;

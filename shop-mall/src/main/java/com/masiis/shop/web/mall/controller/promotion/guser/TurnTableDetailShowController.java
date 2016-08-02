926e90a8c3b8923e0c14e5c9a3e52e16a45da819
quiet516490578 <516490578@qq.com>
2016-08-02 12:00:54 +0800

    修改样式

--- shop-mall/src/main/java/com/masiis/shop/web/mall/controller/promotion/guser/TurnTableDetailShowController.java
+++ shop-mall/src/main/java/com/masiis/shop/web/mall/controller/promotion/guser/TurnTableDetailShowController.java
@@ -1,6 +1,10 @@
 package com.masiis.shop.web.mall.controller.promotion.guser;
 
+import com.masiis.shop.common.enums.promotion.SfTurnTableRuleStatusEnum;
+import com.masiis.shop.common.enums.promotion.SfTurnTableRuleTypeEnum;
+import com.masiis.shop.common.enums.promotion.SfTurnTableStatusEnum;
 import com.masiis.shop.dao.beans.promotion.TurnTablelInfo;
+import com.masiis.shop.web.mall.controller.base.BaseController;
 import com.masiis.shop.web.promotion.cpromotion.service.guser.TurnTableDetailShowService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,13 +18,18 @@ import java.util.List;
  */
 @Controller
 @RequestMapping("/turnTableDetailShow")
-public class TurnTableDetailShowController {
+public class TurnTableDetailShowController extends BaseController {
 
     private TurnTableDetailShowService turnTableDetailShowService;
 
     @RequestMapping("/getTurnTableInfo.html")
     public String getTurnTableInfo(HttpServletRequest request, Model model) {
-        List<TurnTablelInfo> turnTablelInfos =  turnTableDetailShowService.getTurnTableInfo();
-        return null;
+        List<TurnTablelInfo> turnTablelInfos =  turnTableDetailShowService.getTurnTableInfo(
+                getComUser(request),
+                SfTurnTableRuleTypeEnum.C.getCode(),
+                SfTurnTableRuleStatusEnum.EFFECT.getCode(),
+                SfTurnTableStatusEnum.ING.getCode());
+        model.addAttribute("turnTablelInfos",turnTablelInfos);
+        return "promotion/guser/turnTableGiftShow";
     }
 }

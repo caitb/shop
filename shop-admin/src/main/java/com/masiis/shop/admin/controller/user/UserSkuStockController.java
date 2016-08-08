1490c4b23de1739c02349922bb9978a0da360e85
admin <linlin.wang@iimai.com>
2016-08-08 16:13:28 +0800

    合伙人库存

---
+++ shop-admin/src/main/java/com/masiis/shop/admin/controller/user/UserSkuStockController.java
@@ -0,0 +1,34 @@
+package com.masiis.shop.admin.controller.user;
+
+import com.masiis.shop.admin.service.user.UserSkuStockService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by admin on 2016/8/8.
+ */
+@Controller
+@RequestMapping("/userSkuStock")
+public class UserSkuStockController {
+
+    @Resource
+    private UserSkuStockService userSkuStockService;
+
+    @RequestMapping("/list.do")
+    @ResponseBody
+    public Object list(HttpServletRequest request, HttpServletResponse response,
+                              Integer pageNumber,
+                              Integer pageSize,
+                              String sortName,
+                              String sortOrder,
+                              Integer orderStatus) {
+
+        return null;
+
+    }
+}

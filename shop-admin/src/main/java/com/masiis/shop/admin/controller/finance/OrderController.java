4686d2e98c05c0a07ac0fd31ed829e297ec0e95f
caitb <caitb@139.com>
2016-08-25 16:14:54 +0800

    财务统计:店铺订单

---
+++ shop-admin/src/main/java/com/masiis/shop/admin/controller/finance/OrderController.java
@@ -0,0 +1,16 @@
+package com.masiis.shop.admin.controller.finance;
+
+import com.alibaba.druid.support.logging.Log;
+import com.alibaba.druid.support.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by cai_tb on 16/8/25.
+ */
+@Controller
+@RequestMapping("/finance/order")
+public class OrderController {
+
+    private final static Log log = LogFactory.getLog(OrderController.class);
+}

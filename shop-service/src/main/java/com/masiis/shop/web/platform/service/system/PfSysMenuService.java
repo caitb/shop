ef4362dd0b2931c3092db72a12541a53b4f2ddb9
quiet516490578 <516490578@qq.com>
2016-09-05 10:41:26 +0800

    升级时删除pfMenu数据

---
+++ shop-service/src/main/java/com/masiis/shop/web/platform/service/system/PfSysMenuService.java
@@ -0,0 +1,20 @@
+package com.masiis.shop.web.platform.service.system;
+
+import com.masiis.shop.dao.platform.system.PfSysMenuMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by hzz on 2016/9/5.
+ */
+@Service
+public class PfSysMenuService {
+
+    @Resource
+    private PfSysMenuMapper pfSysMenuMapper;
+
+    public int delete(Integer id){
+        return pfSysMenuMapper.deleteByPrimaryKey(id);
+    }
+}

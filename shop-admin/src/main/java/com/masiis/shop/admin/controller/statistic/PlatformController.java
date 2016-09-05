a6bd0eff9a9b2f9a587c640620541a47acdac3db
caitb <caitb@139.com>
2016-09-05 17:35:58 +0800

    数据统计

--- shop-admin/src/main/java/com/masiis/shop/admin/controller/statistic/PlatformController.java
+++ shop-admin/src/main/java/com/masiis/shop/admin/controller/statistic/PlatformController.java
@@ -6,6 +6,7 @@ import com.masiis.shop.admin.service.statistic.StatisticUserService;
 import com.masiis.shop.common.util.WxPropertiesUtils;
 import com.masiis.shop.dao.beans.statistic.StatisticOrder;
 import com.masiis.shop.dao.beans.statistic.StatisticUser;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,44 +38,53 @@ public class PlatformController {
 
     @RequestMapping("/list.do")
     @ResponseBody
-    public Object list(HttpServletRequest request, String beginTime){
+    public Object list(HttpServletRequest request, String beginTime, String endTime){
       try {
-          if(beginTime == null){
+          Date currDate = null;
+          if(StringUtils.isBlank(beginTime)){
+              currDate = new Date();
               SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-              beginTime = sdf.format(new Date())+" 00:00:00";
+              beginTime = sdf.format(currDate) + " 00:00:00";
+              endTime  = sdf.format(currDate)  + " 23:59:59";
+          }else{
+              beginTime = beginTime.split(" ")[0];
+              endTime   = beginTime + " 23:59:59";
+              beginTime = beginTime + " 00:00:00";
           }
+
           //注册用户量
-          List<StatisticUser> userList = statisticUserService.userList(beginTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
+          List<StatisticUser> userList = statisticUserService.userList(beginTime, endTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
           //新增用户量
-          List<StatisticUser> newUserList = statisticUserService.newUserList(beginTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
+          List<StatisticUser> newUserList = statisticUserService.newUserList(beginTime, endTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
           //平台使用用户量（人）（麦链合伙人：合伙人数；）
-          List<StatisticUser> useList = statisticUserService.useList(beginTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
+          List<StatisticUser> useList = statisticUserService.useList(beginTime, endTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
           //平台使用用户量（人）（麦链合伙人：新增合伙人数；）
-          List<StatisticUser> newUseList = statisticUserService.newUseList(beginTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
+          List<StatisticUser> newUseList = statisticUserService.newUseList(beginTime, endTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
           //麦链商城购买人数
-          List<StatisticUser> cityNum = statisticUserService.cityNum(beginTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
+          List<StatisticUser> cityNum = statisticUserService.cityNum(beginTime, endTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
           //麦链商城新增购买人数
-          List<StatisticUser> newCityNum = statisticUserService.newCityNum(beginTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
+          List<StatisticUser> newCityNum = statisticUserService.newCityNum(beginTime, endTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
           //麦链合伙人成交订单量,成交金额
-          List<StatisticOrder> selectOrder = statisticUserService.selectOrder(beginTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
+          List<StatisticOrder> selectOrder = statisticUserService.selectOrder(beginTime, endTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
           //麦链合伙人新增成交订单量,新增成交金额
-          List<StatisticOrder> newSelectOrder = statisticUserService.newSelectOrder(beginTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
+          List<StatisticOrder> newSelectOrder = statisticUserService.newSelectOrder(beginTime, endTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
           //麦链商城<StatisticOrder>成交订单量,成交金额
-          List<StatisticOrder> cityOrder = statisticUserService.cityOrder(beginTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
+          List<StatisticOrder> cityOrder = statisticUserService.cityOrder(beginTime, endTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
           //麦链商城新增成交订单量,新增成交金额
-          List<StatisticOrder> newCityOrder = statisticUserService.newCityOrder(beginTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
+          List<StatisticOrder> newCityOrder = statisticUserService.newCityOrder(beginTime, endTime, WxPropertiesUtils.getStringValue("wx.conf.sf.APPID"));
 
 
           List<Map<String, Object>> list = new ArrayList();
 
           Map<String, Object> user = new HashMap<>();
-         user.put("registerCountUser", userList.get(0).getRegisterCountUser());
-         user.put("registerCount", userList.get(0).getTotal());
-          if(newUserList != null && newUserList.size() > 0){
+          user.put("registerCountUser", userList.get(0).getRegisterCountUser());
+          user.put("registerCount", userList.get(0).getTotal());
+          if(newUserList != null && newUserList.size() == 1 && "麦链合伙人".equals(newUserList.get(0).getRegisterCountUser())){
               user.put("newRegisterCount", newUserList.get(0).getTotal());
-          }else{
-              user.put("newRegisterCount", 0);
+          }else if(newUserList != null && newUserList.size() == 2 && "麦链合伙人".equals(newUserList.get(1).getRegisterCountUser())){
+              user.put("newRegisterCount", userList.get(1).getTotal());
           }
+
           if(useList != null && useList.size() > 0){
               user.put("useCount",useList.get(0).getTotal());
           }else{
@@ -117,11 +127,12 @@ public class PlatformController {
           Map<String, Object> city = new HashMap<>();
           city.put("registerCountUser", userList.get(1).getRegisterCountUser());
           city.put("registerCount", userList.get(1).getTotal());
-          if(newUserList != null && newUserList.size() > 1){
+          if(newUserList != null && newUserList.size() == 1 && "麦链商城".equals(newUserList.get(0).getRegisterCountUser())){
+              city.put("newRegisterCount", newUserList.get(0).getTotal());
+          }else if(newUserList != null && newUserList.size() == 2 && "麦链商城".equals(newUserList.get(1).getRegisterCountUser())){
               city.put("newRegisterCount", newUserList.get(1).getTotal());
-          }else{
-              city.put("newRegisterCount", 0);
           }
+
           if(cityNum != null && cityNum.size() > 0){
               city.put("useCount",cityNum.get(0).getTotal());
           }else{

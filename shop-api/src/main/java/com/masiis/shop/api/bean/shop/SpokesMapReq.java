34da19b2011a47daa64438f1deb0cce7348f40ee
luozhihui <767508395@qq.com>
2016-09-06 20:13:41 +0800

    修改bean

--- shop-api/src/main/java/com/masiis/shop/api/bean/shop/SpokesMapReq.java
+++ shop-api/src/main/java/com/masiis/shop/api/bean/shop/SpokesMapReq.java
@@ -1,12 +1,13 @@
 package com.masiis.shop.api.bean.shop;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.masiis.shop.api.bean.base.BasePagingReq;
 
 /**
  * Created by hw on 2016/7/29.
  */
 public class SpokesMapReq extends BasePagingReq {
-
+    @JSONField(name = "ID")
     private String ID;
 
     public String getID() {

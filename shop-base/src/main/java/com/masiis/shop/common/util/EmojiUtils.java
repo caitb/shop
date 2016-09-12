758be52e9fd8dec2df9a56cfc2f25c85e82de393
luozhihui <767508395@qq.com>
2016-09-12 15:53:31 +0800

    修改解析emoji表情的工具类 系统消息头像修改

--- shop-base/src/main/java/com/masiis/shop/common/util/EmojiUtils.java
+++ shop-base/src/main/java/com/masiis/shop/common/util/EmojiUtils.java
@@ -1,5 +1,7 @@
 package com.masiis.shop.common.util;
 
+import com.vdurmont.emoji.EmojiParser;
+
 /**
  * @Date 2016/5/16
  * @Auther lzh
@@ -9,7 +11,14 @@ public class EmojiUtils {
         if (str == null) {
             return null;
         }
-        str = str.replaceAll("[^\\u0000-\\uFFFF]", "");
+        str = EmojiParser.parseToAliases(str);
         return str;
+    }
+
+    public static void main(String... args) {
+        String str = "An \uD83D\uDE00awesome \uD83D\uDE03string with a few \uD83D\uDE09emojis!";
+        String res = removeNonBmpUnicode(str);
+        System.out.println(res);
+        System.out.println(EmojiParser.parseToUnicode(res));
     }
 }

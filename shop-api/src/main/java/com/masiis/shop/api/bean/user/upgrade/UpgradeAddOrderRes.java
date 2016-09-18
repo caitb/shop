499696b007ee9892c95c2688999256eb5c5064e4
huangwei <wei.huang@iimai.com>
2016-09-18 15:26:50 +0800

    0元升级订单

--- shop-api/src/main/java/com/masiis/shop/api/bean/user/upgrade/UpgradeAddOrderRes.java
+++ shop-api/src/main/java/com/masiis/shop/api/bean/user/upgrade/UpgradeAddOrderRes.java
@@ -9,6 +9,16 @@ public class UpgradeAddOrderRes extends BaseBusinessRes {
 
     private Long orderId;
 
+    private Integer upgradeType;//0 0元升级， 1 非0元升级
+
+    public Integer getUpgradeType() {
+        return upgradeType;
+    }
+
+    public void setUpgradeType(Integer upgradeType) {
+        this.upgradeType = upgradeType;
+    }
+
     public Long getOrderId() {
         return orderId;
     }
